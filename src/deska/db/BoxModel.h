eca3ea9d06ff5e76df535f3feaf6fb2275dea2ed
Jan Kundrát <jkt@gentoo.org>
2010-05-31 00:58:30 +0200

    Split the BoxModel into a Basic class without support for template derivation, and the "regular one"

--- src/deska/db/BoxModel.h
+++ src/deska/db/BoxModel.h
@@ -20,6 +20,78 @@ typedef enum {
 
 DESKA_DECLARE_FLAGS(BayOrderingFlags, BayOrdering)
 
+class BoxModel;
+
+/** @short Storage container for the "boxmodel" statement without templating support
+ *
+ * This class should only store and return information which are explicitly
+ * known at this particular level of template derivation.  All inherited data
+ * shall be requested via the BoxModel class.
+ * */
+class BasicBoxModel {
+public:
+    /** @short Returns the name of the boxmodel kind */
+    virtual std::string name() const;
+    /** @short Returns a pointer to an boxmodel into which this one fits */
+    virtual const BoxModel* fitsIn() const;
+
+    /** @short Returns true if the enclosure dimensions are specified in absolute values */
+    virtual bool hasAbsoluteDimensions() const;
+    /** @short Returns width of the enclosure in milimeters
+     *
+     * Should the dimension be specified in "bay units", the
+     * hasAbsoluteDimensions() returns false, the hasRelativeDimensions()
+     * returns true and functions width(), height() and depth() return -1.
+     */
+    virtual int width() const;
+    /** @short Returns height of the enclosure in milimeters
+     *
+     * @see width()
+     * */
+    virtual int height() const;
+    /** @short Returns depth of the enclosure in milimeters
+     *
+     * @see width()
+     * */
+    virtual int depth() const;
+
+    /** @short Returns true if the outer dimensions of an enclosure are specified in bay units */
+    virtual bool hasRelativeDimensions() const;
+    /** @short Returns number of occupied bays in the "width" direction of the parent rack */
+    virtual int occupiedBaysWidth() const;
+    /** @short Returns number of occupied bays in the "height" direction of the parent rack */
+    virtual int occupiedBaysHeight() const;
+    /** @short Returns number of occupied bays in the "depth" direction of the parent rack */
+    virtual int occupiedBaysDepth() const;
+
+    /** @short Returns true if this boxmodel has inner bay structure */
+    virtual bool hasInnerBays() const;
+    /** @short Number of bays in the "width" direction or -1 if hasInnerBays() is false */
+    virtual int baysWidth() const;
+    /** @short Number of bays in the "height" direction or -1 if hasInnerBays() is false */
+    virtual int baysHeight() const;
+    /** @short Number of bays in the "width" direction or -1 if hasInnerBays() is false */
+    virtual int baysDepth() const;
+    /** @short Order for the bay numbering */
+    virtual BayOrderingFlags bayOrdering() const;
+
+    virtual void setFitsIn( const BoxModel* const where );
+    virtual void setAbsoluteDimensions( const bool areAbsolute );
+    virtual void setWidth( const int num );
+    virtual void setHeight( const int num );
+    virtual void setDepth( const int num );
+    virtual void setOccupiedBaysWidth( const int num );
+    virtual void setOccupiedBaysHeight( const int num );
+    virtual void setOccupiedBaysDepth( const int num );
+    virtual void setBayOrdering( const BayOrderingFlags ordering );
+
+private:
+    // we want to control the lifetime of these objects
+    BasicBoxModel();
+    BasicBoxModel( const BasicBoxModel& );
+    ~BasicBoxModel();
+};
+
 /** @short Representation of the "boxmodel" statement from the CLI
  *
  * Instances of the BoxModel class are managed by the Database object. Users
@@ -27,63 +99,10 @@ DESKA_DECLARE_FLAGS(BayOrderingFlags, BayOrdering)
  * them when needed and won't destroy them until the database itself ceases to
  * exist.
  * */
-class BoxModel {
+class BoxModel: public BasicBoxModel {
 public:
-    /** @short Returns the name of the boxmodel kind */
-    std::string name() const;
-    /** @short Returns a pointer to an boxmodel into which this one fits */
-    const BoxModel* fitsIn() const;
-
-    /** @short Returns true if the enclosure dimensions are specified in absolute values */
-    bool hasAbsoluteDimensions() const;
-    /** @short Returns width of the enclosure in milimeters
-     *
-     * Should the dimension be specified in "bay units", the
-     * hasAbsoluteDimensions() returns false, the hasRelativeDimensions()
-     * returns true and functions width(), height() and depth() return -1.
-     */
-    int width() const;
-    /** @short Returns height of the enclosure in milimeters
-     *
-     * @see width()
-     * */
-    int height() const;
-    /** @short Returns depth of the enclosure in milimeters
-     *
-     * @see width()
-     * */
-    int depth() const;
-
-    /** @short Returns true if the outer dimensions of an enclosure are specified in bay units */
-    bool hasRelativeDimensions() const;
-    /** @short Returns number of occupied bays in the "width" direction of the parent rack */
-    int occupiedBaysWidth() const;
-    /** @short Returns number of occupied bays in the "height" direction of the parent rack */
-    int occupiedBaysHeight() const;
-    /** @short Returns number of occupied bays in the "depth" direction of the parent rack */
-    int occupiedBaysDepth() const;
-
-    /** @short Returns true if this boxmodel has inner bay structure */
-    bool hasInnerBays() const;
-    /** @short Number of bays in the "width" direction or -1 if hasInnerBays() is false */
-    int baysWidth() const;
-    /** @short Number of bays in the "height" direction or -1 if hasInnerBays() is false */
-    int baysHeight() const;
-    /** @short Number of bays in the "width" direction or -1 if hasInnerBays() is false */
-    int baysDepth() const;
-    /** @short Order for the bay numbering */
-    BayOrderingFlags bayOrdering() const;
-
-    void setFitsIn( const BoxModel* const where );
-    void setAbsoluteDimensions( const bool areAbsolute );
-    void setWidth( const int num );
-    void setHeight( const int num );
-    void setDepth( const int num );
-    void setOccupiedBaysWidth( const int num );
-    void setOccupiedBaysHeight( const int num );
-    void setOccupiedBaysDepth( const int num );
-    void setBayOrdering( const BayOrderingFlags ordering );
-
+    BoxModel* extends() const;
+    void setExtends( const BoxModel* const what );
 private:
     // we want to control the lifetime of these objects
     BoxModel();
