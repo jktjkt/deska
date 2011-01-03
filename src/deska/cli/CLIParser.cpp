f37d3738882493e72cf417dd564b615a8c181913
Tomas Hubik <hubik.tomas@gmail.com>
2011-01-03 21:27:01 +0100

    First draft of error handler. Problem with delegating on line 162.

--- src/deska/cli/CLIParser.cpp
+++ src/deska/cli/CLIParser.cpp
@@ -12,7 +12,7 @@
 
 /* Now this parses:
 *
-*  hardware <name> name <quoted string> id <integer> price <double>
+*  hardware <name> name <quoted string> id <integer> price <double> end
 *
 *  Its only an example how this can be handled.
 */
@@ -20,149 +20,194 @@
 
 namespace DeskaCLI
 {
-    namespace qi = boost::spirit::qi;
-    namespace ascii = boost::spirit::ascii;
+	namespace qi = boost::spirit::qi;
+	namespace spirit = boost::spirit;
+	namespace phoenix = boost::phoenix;
+	namespace ascii = boost::spirit::ascii;
 
-    /* Predefined rules for parsing single parameters
-    *  TODO: Rewrite to some singleton or something.
-    */
-    template <typename Iterator>
-    class PredefinedRules
-    {
-    public:
-        PredefinedRules()
-        {
-            t_int %= qi::int_;
-            t_string %= qi::lexeme[ '"' >> +( ascii::char_ - '"' ) >> '"' ];
-            t_double %= qi::double_;
-            identifier %= qi::lexeme[ *( ascii::alnum | '_' ) ];
-        }
 
-        qi::rule< Iterator, int(), ascii::space_type > t_int;
-        qi::rule< Iterator, std::string(), ascii::space_type > t_string;
-        qi::rule< Iterator, double(), ascii::space_type > t_double;
-        qi::rule< Iterator, std::string(), ascii::space_type > identifier;
-    };
+	template < typename Iterator >
+	class ErrorHandler
+	{
+	public:
+		void operator()( Iterator start, Iterator end, Iterator errorPos, const spirit::info& what )
+		{
+			std::cout
+				<< "Error! Expecting " << what
+				<< " here: \"" << phoenix::construct< std::string >( errorPos, end ) << "\""
+				<< std::endl ;
+		}
+	};
 
-    template <typename Iterator>
-    class IfaceGrammar: public qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > >
-    {
-    public:
-        IfaceGrammar() : IfaceGrammar::base_type( start )
-        {
-            using qi::int_;
-            using qi::lit;
-            using qi::double_;
-            using qi::lexeme;
-            using qi::_1;
-            using qi::_a;
-            using qi::_val;
-            using ascii::char_;
 
-            predefined = new PredefinedRules< Iterator >();
+	/* Predefined rules for parsing single parameters
+	*  TODO: Rewrite to some singleton or something.
+	*/
+	template < typename Iterator >
+	class PredefinedRules
+	{
+	public:
+		PredefinedRules()
+		{
+			t_int %= qi::int_;
+			t_int.name( "integer" );
+			
+			t_string %= qi::lexeme[ '"' >> +( ascii::char_ - '"' ) >> '"' ];
+			t_string.name( "quoted string" );
+			
+			t_double %= qi::double_;
+			t_double.name( "double" );
 
-            // Keyword table for matching keywords to parameter types (parser)
-            keyword.add( "name", predefined->t_string );
-            keyword.add( "id", predefined->t_int );
-            keyword.add( "ip", predefined->t_string );
+			identifier %= qi::lexeme[ *( ascii::alnum | '_' ) ];
+			identifier.name( "identifier (alphanumerical letters and _)" );
+		}
 
-            // Head of top-level grammar
-            cat_start %= lit( "interface" ) >> predefined->identifier;
+		qi::rule< Iterator, int(), ascii::space_type > t_int;
+		qi::rule< Iterator, std::string(), ascii::space_type > t_string;
+		qi::rule< Iterator, double(), ascii::space_type > t_double;
+		qi::rule< Iterator, std::string(), ascii::space_type > identifier;
+	};
 
-            // Trick for building the parser during parse time
-            start = cat_start >> +( keyword[ _a = _1 ] >> lazy( _a ) ) >> lit( "end" );
-        }
+	template < typename Iterator >
+	class IfaceGrammar: public qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > >
+	{
+	public:
+		IfaceGrammar() : IfaceGrammar::base_type( start )
+		{
+			using qi::int_;
+			using qi::lit;
+			using qi::double_;
+			using qi::lexeme;
+			using qi::_1;
+			using qi::_a;
+			using qi::_val;
+			using ascii::char_;
 
-        ~IfaceGrammar()
-        {
-            delete predefined;
-        };
+			predefined = new PredefinedRules< Iterator >();
 
-        qi::symbols< char, qi::rule< Iterator, ascii::space_type > > keyword;
-        qi::symbols< char, qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > > nested;
+			// Keyword table for matching keywords to parameter types (parser)
+			keyword.add( "name", predefined->t_string );
+			keyword.add( "id", predefined->t_int );
+			keyword.add( "ip", predefined->t_string );
 
-        qi::rule< Iterator, std::string(), std::string(), ascii::space_type > cat_start;
-        qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > start;
+			// Head of top-level grammar
+			cat_start %= lit( "interface" ) >> predefined->identifier;
 
-        PredefinedRules< Iterator >* predefined;
-    };
+			// Trick for building the parser during parse time
+			start = cat_start >> +( keyword[ _a = _1 ] >> lazy( _a ) ) >> lit( "end" );
+		}
 
-    template <typename Iterator>
-    class MainGrammar: public qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > >
-    {
-    public:
-        MainGrammar() : MainGrammar::base_type( start )
-        {
-            using qi::int_;
-            using qi::lit;
-            using qi::double_;
-            using qi::lexeme;
-            using qi::_1;
-            using qi::_a;
-            using qi::_val;
-            using ascii::char_;
+		~IfaceGrammar()
+		{
+			delete predefined;
+		};
 
-            predefined = new PredefinedRules< Iterator >();
+		qi::symbols< char, qi::rule< Iterator, ascii::space_type > > keyword;
+		qi::symbols< char, qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > > nested;
 
-            // Keyword table for matching keywords to parameter types (parser)
-            keyword.add( "name", predefined->t_string );
-            keyword.add( "id", predefined->t_int );
-            keyword.add( "price", predefined->t_double );
+		qi::rule< Iterator, std::string(), std::string(), ascii::space_type > cat_start;
+		qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > start;
 
-            // TODO: Problem, that grammars are non-copyable objects -> wrapping to phoenix::ref() or something
-            //IfaceGrammar< Iterator > iface = IfaceGrammar< Iterator >();
-            //nested.add( "interface", iface );
+		PredefinedRules< Iterator >* predefined;
+	};
 
-            // Head of top-level grammar
-            cat_start %= lit( "hardware" ) >> predefined->identifier;
+	template <typename Iterator>
+	class MainGrammar: public qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > >
+	{
+	public:
+		MainGrammar() : MainGrammar::base_type( start )
+		{
+			using qi::int_;
+			using qi::lit;
+			using qi::double_;
+			using qi::lexeme;
+			using qi::_1;
+			using qi::_2;
+			using qi::_3;
+			using qi::_4;
+			using qi::_a;
+			using qi::_val;
+			using qi::on_error;
+			using qi::fail;
+			using ascii::char_;
+			using ascii::string;
 
-            // Trick for building the parser during parse time
-            // TODO: Problem, that grammars are non-copyable objects -> wrapping to phoenix::ref() or something
-            start = ( cat_start >> +( ( keyword[ _a = _1 ] >> lazy( _a ) )/* || ( nested[ _a = _1 ] >> lazy( _a ) )*/ ) >> lit( "end" ) );
-        }
+			predefined = new PredefinedRules< Iterator >();
+			errHandler = new ErrorHandler< Iterator >();
 
-        ~MainGrammar()
-        {
-            delete predefined;
-        };
+			// Keyword table for matching keywords to parameter types (parser)
+			keyword.add( "name", predefined->t_string );
+			keyword.add( "id", predefined->t_int );
+			keyword.add( "price", predefined->t_double );
 
-        qi::symbols< char, qi::rule< Iterator, ascii::space_type > > keyword;
-        qi::symbols< char, qi::grammar< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > > nested;
+			// TODO: Problem, that grammars are non-copyable objects -> wrapping to phoenix::ref() or something
+			//qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > iface = IfaceGrammar< Iterator >();
+			//IfaceGrammar< Iterator > iface = IfaceGrammar< Iterator >();
+			//nested.add( "interface", iface );
 
-        qi::rule< Iterator, std::string(), std::string(), ascii::space_type > cat_start;
-        qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > start;
+			// Head of top-level grammar
+			cat_start %= lit( "hardware" ) > predefined->identifier;
+			cat_start.name("cathegory start");
 
-        PredefinedRules< Iterator >* predefined;
-    };
+			// Trick for building the parser during parse time
+			// TODO: Problem, that grammars are non-copyable objects -> wrapping to phoenix::ref() or something
+			start = ( cat_start > +( ( keyword[ _a = _1 ] > lazy( _a ) ) /*|| ( nested[ _a = _1 ] >> lazy( _a ) )*/ ) >> lit( "end" ) );
+
+			on_error< fail >( start, std::cout
+				<< phoenix::val( "Error! Expecting " ) << _4
+				<< phoenix::val( " here: \"" ) << phoenix::construct< std::string >( _3, _2 ) << phoenix::val( "\"" )
+				<< std::endl );
+			
+			//Failed to compile when trying to delegate error handling to separate class
+			//on_error< fail >( start, *errHandler( _1, _2, _3, _4 ) );
+		}
+
+		~MainGrammar()
+		{
+			delete predefined;
+			delete errHandler;
+		};
+
+		qi::symbols< char, qi::rule< Iterator, ascii::space_type > > keyword;
+		qi::symbols< char, qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > > nested;
+		
+		qi::rule< Iterator, std::string(), std::string(), ascii::space_type > cat_start;
+		qi::rule< Iterator, ascii::space_type, qi::locals< qi::rule< Iterator, ascii::space_type > > > start;
+
+		PredefinedRules< Iterator >* predefined;
+		ErrorHandler< Iterator >* errHandler;
+	};
 }
 
 
 
 int main()
 {
-    using boost::spirit::ascii::space;
-    typedef std::string::const_iterator iteratorType;
-    typedef DeskaCLI::MainGrammar< iteratorType > MainGrammar;
+	using boost::spirit::ascii::space;
+	typedef std::string::const_iterator iteratorType;
+	typedef DeskaCLI::MainGrammar< iteratorType > MainGrammar;
 
-    MainGrammar g;
-    std::string str;
-    while ( getline( std::cin, str ) )
-    {
-        if ( str.empty() || str[ 0 ] == 'q' || str[ 0 ] == 'Q' )
-            break;
+	std::cout << "hardware <name> name <quoted string> id <integer> price <double> end" << std::endl;
 
-        std::string::const_iterator iter = str.begin();
-        std::string::const_iterator end = str.end();
-        bool r = phrase_parse( iter, end, g, space );
+	MainGrammar g;
+	std::string str;
+	while ( getline( std::cin, str ) )
+	{
+		if ( str.empty() || str[ 0 ] == 'q' || str[ 0 ] == 'Q' )
+			break;
 
-        if ( r && iter == end )
-        {
-            std::cout << "Parsing succeeded\n" << std::endl;
-        }
-        else
-        {
-            std::cout << "Parsing failed\n";
-        }
-    }
-    return 0;
+		std::string::const_iterator iter = str.begin();
+		std::string::const_iterator end = str.end();
+		bool r = phrase_parse( iter, end, g, space );
+
+		if ( r && iter == end )
+		{
+			std::cout << "Parsing succeeded" << std::endl;
+		}
+		else
+		{
+			std::cout << "Parsing failed" << std::endl;
+		}
+	}
+	return 0;
 }
