25e89ef54c68bb08b4ceef11dd898ee66b4a7151
Jan Kundrát <jkt@gentoo.org>
2011-01-17 00:24:10 +0100

    Fix coding style and a typo in variable name

--- src/deska/db/ApiTest.h
+++ src/deska/db/ApiTest.h
@@ -18,32 +18,32 @@ class ApiTest: public Api
 
     virtual std::vector<Identifier> kindNames() const
     {
-    	std::vector< Identifier > testKindNames;
-    	
-    	testKindNames.push_back("hardware");
-    	testKindNames.push_back("interface");
-    	testKindNames.push_back("host");
-    	
-		return testKindNames;
-	};
+        std::vector< Identifier > testKindNames;
+
+        testKindNames.push_back("hardware");
+        testKindNames.push_back("interface");
+        testKindNames.push_back("host");
+
+        return testKindNames;
+    };
 
     virtual std::vector<KindAttributeDataType> kindAttributes( const Identifier &kindName ) const
     {
-    	std::map< Identifier, std::vector< KindAttributeDataType > > testKindAttirbutes;
-    	
-		testKindAttirbutes["hardware"].push_back( KindAttributeDataType( "id", "integer" ) );
-		testKindAttirbutes["hardware"].push_back( KindAttributeDataType( "name", "string" ) );
-		testKindAttirbutes["hardware"].push_back( KindAttributeDataType( "price", "integer" ) );
-		
-		testKindAttirbutes["interface"].push_back( KindAttributeDataType( "ip", "string" ) );
-		testKindAttirbutes["interface"].push_back( KindAttributeDataType( "mac", "string" ) );
-		
-		testKindAttirbutes["host"].push_back( KindAttributeDataType( "hardware", "identifier" ) );
-		testKindAttirbutes["host"].push_back( KindAttributeDataType( "name", "string" ) );
-    	
-    	return testKindAttirbutes[ kindName ];
-	};
-	
+        std::map< Identifier, std::vector< KindAttributeDataType > > testKindAttributes;
+
+        testKindAttributes["hardware"].push_back( KindAttributeDataType( "id", "integer" ) );
+        testKindAttributes["hardware"].push_back( KindAttributeDataType( "name", "string" ) );
+        testKindAttributes["hardware"].push_back( KindAttributeDataType( "price", "integer" ) );
+
+        testKindAttributes["interface"].push_back( KindAttributeDataType( "ip", "string" ) );
+        testKindAttributes["interface"].push_back( KindAttributeDataType( "mac", "string" ) );
+
+        testKindAttributes["host"].push_back( KindAttributeDataType( "hardware", "identifier" ) );
+        testKindAttributes["host"].push_back( KindAttributeDataType( "name", "string" ) );
+
+        return testKindAttributes[ kindName ];
+    };
+
 };
 
 
