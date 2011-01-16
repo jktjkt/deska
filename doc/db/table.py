27d77d2ff1afb9e9bf07528c1cd14a141bd13446
Lukáš Kerpl <luke@minutux.tuxsite>
2011-01-16 22:52:51 +0100

    Now we can generate these functions: add - for adding row in historic table del - for deleting from production table (mark as deleted in historic table) - need some improvments set_column And also generate historic table and commit function (this is not completed)

--- doc/db/table.py
+++ doc/db/table.py
@@ -1,22 +1,76 @@
-#!/usr/bin/python2.4
+#!/usr/bin/python2
 
 class Table:
+	# template string for generate historic table
 	hist_string = '''CREATE TABLE {0[name]}_history (
-		LIKE {0[name]},
-		version int NOT NULL,
-		dest_bit bit(1) NOT NULL DEFAULT B'0',
-		CONSTRAINT {0[name]}_history_pk PRIMARY KEY (uid,version)
-		)'''
+	LIKE {0[name]},
+	version int NOT NULL,
+	dest_bit bit(1) NOT NULL DEFAULT B'0',
+	CONSTRAINT {0[name]}_history_pk PRIMARY KEY (uid,version)
+	)'''
+	# template string for set function's
 	set_string = '''CREATE FUNCITION
-		{0[name]}_{1}_set(IN id integer,IN value {2})
-		RETURNS interger
-		AS
-		$$
-			UPDATE TABLE {0[name]}_history SET {1} = value
-				WHERE uid = id
-		$$
-		LAGUAGE plpsql;
-		'''
+	{0[name]}_set_{1}(IN id integer,IN value {2},IN ver integer)
+	RETURNS interger
+	AS
+	$$
+	BEGIN
+		UPDATE TABLE {0[name]}_history SET {1} = value
+			WHERE uid = id;
+		RETURN 1;
+	END
+	$$
+	LAGUAGE plpsql;
+	'''
+	# template string for add function
+	add_string = '''CREATE FUNCITION
+	{0[name]}_add(IN name text,IN ver integer)
+	RETURNS integer
+	AS
+	$$
+	BEGIN
+		INSERT INTO {0[name]}_history (name,version)
+			VALUES (value,ver);
+		RETURN 1;
+	END
+	$$
+	LAGUAGE plpsql;
+	'''
+	# template string for del function
+	del_string = '''CREATE FUNCITION
+	{0[name]}_del(IN id integer,IN ver integer)
+	RETURNS integer
+	AS
+	$$
+	BEGIN
+		INSERT INTO vendor_version (uid, version, dest_bit)
+			VALUES (id, name, ver, '1');
+		RETURN 1;
+	END
+	$$
+	LAGUAGE plpsql;
+	'''
+	# template string for commit function
+	commit_string = '''CREATE FUNCITION
+	{0[name]}_commit(IN ver integer)
+	RETURNS integer
+	AS
+	$$
+	BEGIN
+		UPDATE {0[name]} as v SET name = new.name
+			FROM {0[name]}_version as new
+				WHERE version = ver AND v.uid = new.uid;
+		INSERT INTO {0[name]} (uid,name)
+			SELECT uid,name FROM {0[name]}_version
+				WHERE version = ver AND uid NOT IN ( SELECT uid FROM {0[name]} );
+		DELETE FROM {0[name]}
+			WHERE uid IN (SELECT uid FROM {0[name]}_version
+				WHERE version = ver AND dest_bit = '1');
+		RETURN 1;
+	END
+	$$
+	LAGUAGE plpsql;
+	'''
 	def __init__(self,name):
 		self.data = dict()
 		self.col = dict()
@@ -28,18 +82,25 @@ class Table:
 	def gen_hist(self):
 		return self.hist_string.format(self.data)
 
-	def gen_add(self,col_name):
-		return 'Not implemented yet'
+	def gen_add(self):
+		return self.add_string.format(self.data)
 
-	def gen_del(self,col_name):
-		return 'Not implemented yet'
+	def gen_del(self):
+		return self.del_string.format(self.data)
 
 	def gen_set(self,col_name):
 		return self.set_string.format(self.data,col_name,self.col[col_name])
+
+	def gen_commit(self):
+		#TODO if there is more columns...
+		return self.commit_string.format(self.data)
 
 # just testing it
 vendor = Table('vendor')
 vendor.add_column('name','text')
 print vendor.gen_hist()
 print vendor.gen_set('name')
+print vendor.gen_add()
+print vendor.gen_del()
+print vendor.gen_commit()
 
