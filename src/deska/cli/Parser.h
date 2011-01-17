7a164aab20d2e4c8ed0f2a2312ae80324feb0947
Jan Kundrát <jkt@gentoo.org>
2011-01-17 12:38:23 +0100

    Fixes for build and coding style issues
    System headers shall be sorted alphabetically and referenced before custom
    headers.

--- src/deska/cli/Parser.h
+++ src/deska/cli/Parser.h
@@ -1,23 +1,19 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-
-
-#include <../db/TestApi.h>
-
-#include <boost/config/warning_disable.hpp>
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_object.hpp>
-#include <boost/lambda/lambda.hpp>
-#include <boost/fusion/include/io.hpp>
-
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include <boost/config/warning_disable.hpp>
+#include <boost/fusion/include/io.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
 
+#include "deska/db/ApiTest.h"
 
 namespace DeskaCLI
 {
