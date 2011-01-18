16a6d7471ba62bc96af7c1be22f8d085fb19b1c2
Jan Kundrát <jkt@gentoo.org>
2011-01-18 10:27:46 +0100

    Add libDeskaDb, at this point just with an incomplete fake implementation

---
+++ src/deska/db/FakeApi.cpp
@@ -0,0 +1,88 @@
+/* Copyright (C) 2011 Jan Kundrát <kundratj@fzu.cz>
+*
+* This file is part of the Deska, a tool for central administration of a grid site
+* http://projects.flaska.net/projects/show/deska
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or the version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+* */
+
+#include <algorithm>
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/construct.hpp>
+#include "FakeApi.h"
+
+using namespace std;
+
+namespace Deska {
+
+FakeApi::FakeApi()
+{
+}
+
+FakeApi::~FakeApi()
+{
+}
+
+vector<Identifier> FakeApi::kindNames() const
+{
+    using namespace boost::lambda;
+    vector<Identifier> res;
+    transform( attrs.begin(), attrs.end(), back_inserter(res),
+               bind( &map<string, vector<KindAttributeDataType> >::value_type::first, _1 )
+               );
+    return res;
+}
+
+vector<KindAttributeDataType> FakeApi::kindAttributes( const Identifier &kindName ) const
+{
+    map<string, vector<KindAttributeDataType> >::const_iterator it = attrs.find( kindName );
+    if ( it == attrs.end() )
+        return vector<KindAttributeDataType>();
+    else
+        return it->second;
+}
+
+vector<ObjectRelation> FakeApi::kindRelations( const Identifier &kindName ) const
+{
+    map<string, vector<ObjectRelation> >::const_iterator it = relations.find( kindName );
+    if ( it == relations.end() )
+        return vector<ObjectRelation>();
+    else
+        return it->second;
+}
+
+/*vector<Identifier> FakeApi::kindInstances( const Identifier &kindName, const Revision=0 ) const;
+map<Identifier, Value> FakeApi::objectData( const Identifier &kindName, const Identifier &objectName, const Revision=0 );
+map<Identifier, pair<Identifier, Value> > FakeApi::resolvedObjectData(
+const Identifier &kindName, const Identifier &objectName, const Revision=0 );
+vector<Identifier> FakeApi::findOverriddenAttrs( const Identifier &kindName, const Identifier &objectName,
+const Identifier &attrName );
+vector<Identifier> FakeApi::findNonOverriddenAttrs( const Identifier &kindName, const Identifier &objectName,
+const Identifier &attrName );
+
+void FakeApi::deleteObject( const Identifier &kindName, const Identifier &objectName );
+void FakeApi::createObject( const Identifier &kindName, const Identifier &objectname );
+void FakeApi::renameObject( const Identifier &kindName, const Identifier &oldName, const Identifier &newName );
+void FakeApi::removeAttribute( const Identifier &kindName, const Identifier &objectName,
+const Identifier &attributeName );
+void FakeApi::setAttribute( const Identifier &kindName, const Identifier &objectName,
+const Identifier &attributeName, const Value &value );
+
+void FakeApi::startChangeset();
+void FakeApi::commit();
+void FakeApi::rebaseTransaction( const Revision rev );*/
+
+}
---
+++ src/deska/db/FakeApi.h
@@ -0,0 +1,77 @@
+/* Copyright (C) 2011 Jan Kundrát <kundratj@fzu.cz>
+*
+* This file is part of the Deska, a tool for central administration of a grid site
+* http://projects.flaska.net/projects/show/deska
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or the version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+* */
+
+#ifndef DESKA_FAKEAPI_H
+#define DESKA_FAKEAPI_H
+
+#include "Api.h"
+
+namespace Deska {
+
+/** @short A fake implementation of the DB API for testing purposes */
+class FakeApi: public Api {
+public:
+    FakeApi();
+    virtual ~FakeApi();
+
+    // Querying schema definition
+    virtual std::vector<Identifier> kindNames() const;
+    virtual std::vector<KindAttributeDataType> kindAttributes( const Identifier &kindName ) const;
+    virtual std::vector<ObjectRelation> kindRelations( const Identifier &kindName ) const;
+
+    // Returning data for existing objects
+    virtual std::vector<Identifier> kindInstances( const Identifier &kindName, const Revision=0 ) const;
+    virtual std::map<Identifier, Value> objectData( const Identifier &kindName, const Identifier &objectName, const Revision=0 );
+    virtual std::map<Identifier, std::pair<Identifier, Value> > resolvedObjectData(
+            const Identifier &kindName, const Identifier &objectName, const Revision=0 );
+    virtual std::vector<Identifier> findOverriddenAttrs( const Identifier &kindName, const Identifier &objectName,
+            const Identifier &attrName );
+    virtual std::vector<Identifier> findNonOverriddenAttrs( const Identifier &kindName, const Identifier &objectName,
+            const Identifier &attrName );
+
+    // Manipulating objects
+    virtual void deleteObject( const Identifier &kindName, const Identifier &objectName );
+    virtual void createObject( const Identifier &kindName, const Identifier &objectname );
+    virtual void renameObject( const Identifier &kindName, const Identifier &oldName, const Identifier &newName );
+    virtual void removeAttribute( const Identifier &kindName, const Identifier &objectName,
+            const Identifier &attributeName );
+    virtual void setAttribute( const Identifier &kindName, const Identifier &objectName,
+            const Identifier &attributeName, const Value &value );
+
+    // SCM-like operation and transaction control
+    virtual void startChangeset();
+    virtual void commit();
+    virtual void rebaseTransaction( const Revision rev );
+
+    // These members should be accessible for modifications from the test suite
+
+    /** @short Attributes of various top-level objects */
+    std::map<std::string, std::vector<KindAttributeDataType> > attrs;
+
+    /** @short Relations, as "retrieved" from the DB scheme */
+    std::map<std::string, std::vector<ObjectRelation> > relations;
+
+    // FIXME: add more members for implementing other methods
+};
+
+}
+
+#endif // DESKA_FAKEAPI_H
