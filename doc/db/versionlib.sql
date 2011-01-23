40fdba64a6243f214cc66e55c9f481553494f115
Lukáš Kerpl <luke@minitux>
2011-01-23 23:51:54 +0100

    Add create version 	modified:   TODO.txt 	modified:   versionlib.sql

--- doc/db/versionlib.sql
+++ doc/db/versionlib.sql
@@ -6,16 +6,34 @@ DROP TABLE version;
 -- vendors of hw -- versioning table
 CREATE TABLE version (
 	-- internal id
-	id int
+	id bigserial
 		CONSTRAINT version_pk PRIMARY KEY,
 	-- human readable id
 	number int
 		CONSTRAINT version_number_unique UNIQUE,
-	created timestamp  NOT NULL
+	created timestamp without time zone NOT NULL default 'now',
+	note text
 );
 GRANT ALL ON version TO deska_team;
 
 --
 -- functions
 --
+
+--
+-- fuction for create version - and return it's number
+--
+CREATE OR REPLACE FUNCTION add_version()
+RETURNS integer
+AS
+$$
+DECLARE ret integer;
+BEGIN
+	INSERT INTO version (note)
+		VALUES ('');
+	SELECT max(id) INTO ret FROM version;
+	RETURN ret;
+END
+$$
+LANGUAGE plpgsql;
 
