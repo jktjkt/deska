ca18d443856cfeb5439d61e0dd137e51dab72af8
Lukáš Kerpl <luke@minitux>
2011-01-24 12:20:22 +0100

    File foc recreating whole db 	new file:   create.sql

---
+++ src/deska/server/db/create.sql
@@ -0,0 +1,13 @@
+-- create schemas
+\i create_schemas.sql
+
+-- create version table and function
+\i versionlib.sql
+
+-- create functions
+\i fn-get-schema.sql
+
+-- add modules
+\i modules/vendor.sql
+
+
