8c6c3f136b5cb30b487876777c9c77d96b255f22
Lukáš Kerpl <luke7@seznam.cz>
2011-02-01 00:11:36 +0100

    Add removeAttribute 	modified:   wrapper.py

--- src/deska/server/app/wrapper.py
+++ src/deska/server/app/wrapper.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python
+
+# import api for generated stored procedures
 import db
 
 #
-# wrapper for
-# 
-# /** @short Set an attribute that belongs to some object to a new value */
-# virtual void setAttribute( const Identifier &kindName, const Identifier &objectName,
-# const Identifier &attributeName, const Value &value ) = 0;
+# auxiliary functions ... 
+#
 
 # strings
-set_string = "{0}_set_{1}"
+fn3_string = "{0}_{1}_{2}"
 fn_string = "{0}_{1}"
 
 def getfn(fnType, kindName,attributeName = ""):
-	if fnType == "set":
-		fname = set_string.format(kindName,attributeName)	
+	if (fnType == "set") or  (fnType == "rem") :
+		fname = fn3_string.format(kindName,fnType,attributeName)	
 	else:
 		fname = fn_string.format(kindName,fnType)
 	fn = getattr(db,fname)
 	return fn
 
+# 
+# functions implementing API
+#
 def setAttribute(kindName,objectName,attributeName,value):
 	fn= getfn("set",kindName,attributeName)
+	return fn(objectName, value)
+
+def removeAttribute(kindName,objectName,attributeName,value):
+	fn= getfn("rem",kindName,attributeName)
 	return fn(objectName, value)
 
 def changeObjectName(kindName,oldName,newName):
@@ -36,6 +42,7 @@ def deleteObject(kindName,objectName):
 	fn = getfn("del",kindName)
 	return fn(objectName)
 
+# pre-test ...
 print setAttribute("vendor","DELL","note","this is note")
 print changeObjectName("vendor","DELL","Dell")
 print createObject("vendor","DELL")
