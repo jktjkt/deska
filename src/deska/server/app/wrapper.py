769fbddf27160d16eac4da0f6785001d3a254f46
Lukáš Kerpl <luke7@seznam.cz>
2011-02-01 00:00:05 +0100

    More work on wrapper - include generated lib 	modified:   wrapper.py

--- src/deska/server/app/wrapper.py
+++ src/deska/server/app/wrapper.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+import db
 
 #
 # wrapper for
@@ -11,51 +12,32 @@
 set_string = "{0}_set_{1}"
 fn_string = "{0}_{1}"
 
-def getfname(fnType, kindName):
-	fn = fn_string.format(kindName,fnType)	
-	return fn
-
-def getfname(fnType, kindName,attributeName = ""):
+def getfn(fnType, kindName,attributeName = ""):
 	if fnType == "set":
-		fn = set_string.format(kindName,attributeName)	
+		fname = set_string.format(kindName,attributeName)	
 	else:
-		# maybe we dont need this
-		fn = fn_string.format(kindName,fnType)
+		fname = fn_string.format(kindName,fnType)
+	fn = getattr(db,fname)
 	return fn
-
-def vendor_add(objectName):
-	return "vendor_add({0})".format(objectName)
-
-def vendor_del(objectName):
-	return "vendor_del({0})".format(objectName)
-
-def vendor_set_note(objectName, value):
-	return "vendor_set_note({0},{1})".format(objectName, value)
-
-def vendor_set_name(objectName, value):
-	return "vendor_set_name({0},{1})".format(objectName, value)
 
 def setAttribute(kindName,objectName,attributeName,value):
-	fname = getfname("set",kindName,attributeName)
-	fn = globals()[fname]
+	fn= getfn("set",kindName,attributeName)
 	return fn(objectName, value)
 
 def changeObjectName(kindName,oldName,newName):
-	fname = getfname("set",kindName,"name")
-	fn = globals()[fname]
+	fn = getfn("set",kindName,"name")
 	return fn(oldName, newName)
 
 def createObject(kindName,objectName):
-	fname = getfname("add",kindName)
-	fn = globals()[fname]
+	fn = getfn("add",kindName)
 	return fn(objectName)
 
 def deleteObject(kindName,objectName):
-	fname = getfname("del",kindName)
-	fn = globals()[fname]
+	fn = getfn("del",kindName)
 	return fn(objectName)
 
 print setAttribute("vendor","DELL","note","this is note")
 print changeObjectName("vendor","DELL","Dell")
 print createObject("vendor","DELL")
 print deleteObject("vendor","DELL")
+
