a7ea264ba5f25e9f823bf83a41e030f0072998f1
Lukáš Kerpl <luke7@seznam.cz>
2011-02-11 21:46:05 +0100

    Add create changeset fuctionality 	modified:   versionlib.sql

--- src/deska/server/db/versionlib.sql
+++ src/deska/server/db/versionlib.sql
@@ -14,6 +14,18 @@ CREATE TABLE version (
 );
 GRANT ALL ON version TO deska_team;
 
+-- current changesets
+CREATE TABLE changeset (
+	-- number - id version
+	version bigint
+		CONSTRAINT session_id_fk_version REFERENCES version(id),
+	-- user - must be unique, use it for pk
+	username text
+		CONSTRAINT session_pk PRIMARY KEY
+);
+
+
+
 --
 -- functions
 --
@@ -21,7 +33,7 @@ GRANT ALL ON version TO deska_team;
 --
 -- fuction for create version - and return it's number
 --
-CREATE OR REPLACE FUNCTION add_version()
+CREATE FUNCTION add_version()
 RETURNS integer
 AS
 $$
@@ -35,3 +47,35 @@ END
 $$
 LANGUAGE plpgsql;
 
+--
+-- start changeset
+--
+CREATE FUNCTION start_changeset()
+RETURNS integer
+AS
+$$
+DECLARE ver integer;
+BEGIN
+	SELECT add_version() INTO ver;
+	INSERT INTO changeset (username,version)
+		VALUES (current_user,ver);
+	RETURN 1;
+END
+$$
+LANGUAGE plpgsql;
+
+--
+-- get my version id
+--
+CREATE FUNCTION my_version()
+RETURNS integer
+AS
+$$
+DECLARE ver integer;
+BEGIN
+	SELECT version INTO ver FROM changeset
+		WHERE username = current_user;
+	RETURN ver;
+END
+$$
+LANGUAGE plpgsql;
