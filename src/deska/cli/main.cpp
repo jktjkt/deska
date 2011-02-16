70d8610cc199904d526a560d2ac2708c91398ebe
Jan Kundrát <jkt@gentoo.org>
2011-02-16 12:09:49 +0100

    New API for the Parser
    refs #53 @2h

--- src/deska/cli/main.cpp
+++ src/deska/cli/main.cpp
@@ -1,44 +1,6 @@
 #include "Parser.h"
-#include "deska/db/FakeApi.h"
-
-
-
-/* Now this parses:
-*
-*  hardware <name> name <quoted string> id <integer> price <double> end
-*
-*  Its only an example how this can be handled.
-*/
-
-
 
 int main()
 {
-    using boost::spirit::ascii::space;
-    typedef std::string::const_iterator iteratorType;
-    typedef Deska::CLI::HardwareGrammar< iteratorType > HardwareGrammar;
-
-    std::cout << "hardware <name> name <quoted string> id <integer> price <double> end" << std::endl;
-
-    HardwareGrammar g;
-    std::string str;
-    while ( getline( std::cin, str ) )
-    {
-        if ( str.empty() || str[ 0 ] == 'q' || str[ 0 ] == 'Q' )
-            break;
-
-        std::string::const_iterator iter = str.begin();
-        std::string::const_iterator end = str.end();
-        bool r = phrase_parse( iter, end, g, space );
-
-        if ( r && iter == end )
-        {
-            std::cout << "Parsing succeeded" << std::endl;
-        }
-        else
-        {
-            std::cout << "Parsing failed" << std::endl;
-        }
-    }
     return 0;
 }
