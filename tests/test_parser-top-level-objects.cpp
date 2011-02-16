2115afba267162f91e7509519f5d29b50a255bc8
Jan Kundrát <jkt@gentoo.org>
2011-02-16 21:15:11 +0100

    Implement unit test stubs for the Parser
    Refs #53 @1h15min

--- tests/test_parser-top-level-objects.cpp
+++ tests/test_parser-top-level-objects.cpp
@@ -19,13 +19,83 @@
 * Boston, MA 02110-1301, USA.
 * */
 
+#include <queue>
 #define BOOST_TEST_MODULE example
 #include <boost/test/unit_test.hpp>
 
 #include "deska/db/FakeApi.h"
 #include "deska/cli/Parser.h"
 
-struct F {
+/** @short Helper class representing a signal emitted by the Parser being tested */
+class MockParserEvent
+{
+public:
+    /** @short The categoryEntered() signal */
+    static MockParserEvent categoryEntered(const Deska::Identifier &kind, const Deska::Identifier &name)
+    {
+        MockParserEvent res(EVENT_ENTER_CONTEXT);
+        res.i1 = kind;
+        res.i2 = name;
+        return res;
+    }
+
+    /** @short The categoryLeft() signal */
+    static MockParserEvent categoryLeft()
+    {
+        return MockParserEvent(EVENT_LEAVE_CONTEXT);
+    }
+
+    /** @short The setAttr() signal */
+    static MockParserEvent setAttr(const Deska::Identifier &name, const Deska::Value &val)
+    {
+        MockParserEvent res(EVENT_SET_ATTR);
+        res.i1 = name;
+        res.v1 = val;
+        return res;
+    }
+
+    bool operator==(const MockParserEvent &other) const
+    {
+        return eventKind == other.eventKind && i1 == other.i1 && i2 == other.i2 && v1 == other.v1;
+    }
+
+    friend std::ostream& operator<<(std::ostream &out, const MockParserEvent &m);
+
+private:
+    typedef enum {
+        /** @short Handler for the categoryEntered() signal */
+        EVENT_ENTER_CONTEXT,
+        /** @short handler for categoryLeft() */
+        EVENT_LEAVE_CONTEXT,
+        /** @short Handler for setAttribute() */
+        EVENT_SET_ATTR
+    } Event;
+
+    MockParserEvent(Event e): eventKind(e) {}
+
+    Event eventKind;
+    Deska::Identifier i1, i2;
+    Deska::Value v1;
+};
+
+std::ostream& operator<<(std::ostream &out, const MockParserEvent &m)
+{
+    switch (m.eventKind) {
+    case MockParserEvent::EVENT_ENTER_CONTEXT:
+        out << "categoryEntered( " << m.i1 << ", " << m.i2 << " )";
+        break;
+    case MockParserEvent::EVENT_LEAVE_CONTEXT:
+        out << "categoryLeft()";
+        break;
+    case MockParserEvent::EVENT_SET_ATTR:
+        out << "setAttr( " << m.i1 << ", " << m.v1 << " )";
+        break;
+    }
+    return out;
+}
+
+struct F
+{
     F()
     {
         using namespace Deska;
@@ -46,11 +116,94 @@ struct F {
         delete db;
     }
 
+    /** @short Handler for Parser's categoryEntered signal */
+    void slotParserCategoryEntered(const Deska::Identifier &kind, const Deska::Identifier &name)
+    {
+        parserEvents.push(MockParserEvent::categoryEntered(kind, name));
+    }
+
+    /** @short Handler for Parser's categoryLeft() */
+    void slotParserCategoryLeft()
+    {
+        parserEvents.push(MockParserEvent::categoryLeft());
+    }
+
+    /** @short Handler for Parser's setAttr() signal */
+    void slotParserSetAttr(const Deska::Identifier &name, const Deska::Value &val)
+    {
+        parserEvents.push(MockParserEvent::setAttr(name, val));
+    }
+
+    /** @short Call this function to verify that no more events were logged */
+    void expectNothingElse()
+    {
+        BOOST_CHECK_MESSAGE(parserEvents.empty(), "Expected no more emitted signals");
+    }
+
+    /** @short Verify that the first signal which wasn't checked yet was the categoryEntered and that its argument match */
+    void expectCategoryEntered(const Deska::Identifier &kind, const Deska::Identifier &name)
+    {
+        expectHelper(MockParserEvent::categoryEntered(kind, name));
+    }
+
+    /** @short Check for categoryLeft
+
+    @see expectCategoryEntered()
+    */
+    void expectCategoryLeft()
+    {
+        expectHelper(MockParserEvent::categoryLeft());
+    }
+
+    /** @short Check that the first signal which was not checked yet was the setAttr()
+
+    @see expectCategoryEntered();
+    */
+    void expectSetAttr(const Deska::Identifier &name, const Deska::Value &val)
+    {
+        expectHelper(MockParserEvent::setAttr(name, val));
+    }
+
+    /** @short Helper for various expect* functions */
+    void expectHelper(const MockParserEvent &e)
+    {
+        BOOST_REQUIRE( ! parserEvents.empty() );
+        MockParserEvent other = parserEvents.front();
+        BOOST_CHECK_EQUAL(e, other);
+        parserEvents.pop();
+    }
+
     Deska::Api *db;
+    std::queue<MockParserEvent> parserEvents;
 };
 
+
+/** @short Verify that the mock object setup works properly
+
+In this test case, we check our mock facility for obvious errors.
+*/
+BOOST_FIXTURE_TEST_CASE( test_mock_objects, F )
+{
+    // At first, nothing should be present in there
+    expectNothingElse();
+
+    // Simulate slots triggered by the Parser
+    slotParserCategoryEntered("a", "b");
+    slotParserSetAttr("foo", "bar");
+    slotParserCategoryLeft();
+
+    // ...and verify that we indeed received them
+    expectCategoryEntered("a", "b");
+    expectSetAttr("foo", "bar");
+    expectCategoryLeft();
+
+    // The shouldn't be anything else at this point
+    expectNothingElse();
+}
 
 BOOST_FIXTURE_TEST_CASE( parsing_top_level_objects, F )
 {
     Deska::CLI::Parser parser(db);
+
+    // FIXME: real test goes here
 }
