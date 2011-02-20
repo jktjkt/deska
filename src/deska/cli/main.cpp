295d4d8e6dd101bfd8a49d5058f6624134c076cc
Tomas Hubik <hubik.tomas@gmail.com>
2011-02-20 22:58:01 +0100

    part of code causing compilation failure in gcc commented out

--- src/deska/cli/main.cpp
+++ src/deska/cli/main.cpp
@@ -6,21 +6,25 @@
 
 int main()
 {
-    Deska::FakeApi *dbApi = new Deska::FakeApi();
+    /* FIXME: undefined reference to `Deska::FakeApi::FakeApi()' in gcc
+    using namespace Deska;
 
-    dbApi->attrs["hardware"].push_back( Deska::KindAttributeDataType( "id", "integer" ) );
-    dbApi->attrs["hardware"].push_back( Deska::KindAttributeDataType( "name", "quoted_string" ) );
-    dbApi->attrs["hardware"].push_back( Deska::KindAttributeDataType( "price", "double" ) );
-    dbApi->attrs["interface"].push_back( Deska::KindAttributeDataType( "ip", "quoted_string" ) );
-    dbApi->attrs["interface"].push_back( Deska::KindAttributeDataType( "mac", "quoted_string" ) );
-    dbApi->attrs["host"].push_back( Deska::KindAttributeDataType( "hardware", "identifier" ) );
-    dbApi->attrs["host"].push_back( Deska::KindAttributeDataType( "name", "quoted_string" ) );
+    FakeApi *fake = new FakeApi();
 
-    Deska::CLI::Parser<Deska::CLI::iterator_type> parser( dbApi );
+    fake->attrs["hardware"].push_back( KindAttributeDataType( "id", "integer" ) );
+    fake->attrs["hardware"].push_back( KindAttributeDataType( "name", "quoted_string" ) );
+    fake->attrs["hardware"].push_back( KindAttributeDataType( "price", "double" ) );
+    fake->attrs["interface"].push_back( KindAttributeDataType( "ip", "quoted_string" ) );
+    fake->attrs["interface"].push_back( KindAttributeDataType( "mac", "quoted_string" ) );
+    fake->attrs["host"].push_back( KindAttributeDataType( "hardware", "identifier" ) );
+    fake->attrs["host"].push_back( KindAttributeDataType( "name", "quoted_string" ) );
+
+    Deska::CLI::Parser<CLI::iterator_type> parser( fake );
 
     std::string test = "hardware abcde id 123 name \"jmeno\" price 1234.5";
 
     parser.parseLine( test );
+    */
     
     /*
     while ( getline( std::cin, str ) )
