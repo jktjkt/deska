cdff5aab80e72a20ca7f7f8fa1464c2b995a986d
Lukáš Kerpl <luke7@seznam.cz>
2011-02-27 11:06:11 +0100

    Minor fixes 	modified:   src/deska/server/db/modules/hardware.sql 	modified:   src/deska/server/db/modules/vendor.sql

--- src/deska/server/db/modules/hardware.sql
+++ src/deska/server/db/modules/hardware.sql
@@ -12,8 +12,20 @@ CREATE TABLE hardware (
 	name text
 		CONSTRAINT hardware_name_unique UNIQUE NOT NULL,
 	vendor bigint
-		REFERENCES vendor(uid)
-
+		REFERENCES vendor(uid),
+	purchase date NOT NULL,
+	warranty date NOT NULL,
+	-- GB of RAM
+	ram INT
+		CONSTRAINT hardware_ram_positive
+		CHECK (ram > 0),
+	-- number of CPU's
+	cpu_num INT
+		CONSTRAINT hardware_cpu_num_positive
+		CHECK (cpu_num > 0),
+	-- add cpu type, when we have cpu type table
+	note text
 );
 GRANT ALL ON hardware TO deska_team;
+
 
