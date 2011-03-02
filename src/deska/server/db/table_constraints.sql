dbb90746d43d3c7508bc902d0c7ad4bded6cac04
Martina Krejcova <martinka.krejcova@seznam.cz>
2011-03-02 11:05:06 +0000

    table constraints - pk,fk,unique,check

--- src/deska/server/db/table_constraints.sql
+++ src/deska/server/db/table_constraints.sql
@@ -1,12 +1,63 @@
-CREATE TYPE constraints_on_table_type AS(
+CREATE TYPE pk_constraints_on_table_type AS(
+conname	name,
+attname	name
+);
+
+CREATE OR REPLACE FUNCTION pk_constraints_on_table(tabname name)
+RETURNS SETOF pk_constraints_on_table_type
+AS
+$$
+DECLARE
+	--contraint name
+	cname name;
+	--counter of pk columns
+	counter int;
+	--name of currently processeding column name
+	currname name;
+	--list of column ids
+	idarray smallint[];
+	--class oid of table
+	coid int;
+	--one row of result (constraint_name, columns_names, referenced_table_name, referenced_columns_names)
+	r pk_constraints_on_table_type;
+BEGIN
+	--select primary key constraint on this table
+	SELECT conname, class.oid, conkey INTO cname, coid, idarray
+	FROM	pg_constraint AS constr
+		--join with TABLE which the contraint is ON
+		join pg_class AS class ON (constr.conrelid = class.oid)	
+	WHERE class.relname=tabname AND contype='p';
+
+	counter = array_lower(idarray, 1);
+		
+	WHILE (counter <= array_upper(idarray, 1)) LOOP
+		--selecting columns
+		SELECT attname INTO currname
+		FROM pg_attribute AS att
+		WHERE att.attrelid = coid AND att.attnum = idarray[counter];
+		r = ROW(cname, currname);
+		RETURN NEXT r;
+		counter = counter + 1;
+	END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
+--SELECT pk_constraints_on_table('test');
+
+
+
+CREATE TYPE fk_constraints_on_table_type AS(
 conname	name,
 attname	name,
 reftabname	name,
 refattname	name
 );
 
-CREATE OR REPLACE FUNCTION constraints_on_table(tabname name)
-RETURNS SETOF constraints_on_table_type
+--DROP FUNCTION fk_constraints_on_table(name);
+
+CREATE OR REPLACE FUNCTION fk_constraints_on_table(tabname name)
+RETURNS SETOF fk_constraints_on_table_type
 AS
 $$
 DECLARE
@@ -29,36 +80,18 @@ DECLARE
 	--class oid of referenced table
 	refcoid int;
 	--one row of result (constraint_name, columns_names, referenced_table_name, referenced_columns_names)
-	r constraints_on_table_type;
+	r fk_constraints_on_table_type;
 BEGIN
-	--select primary key constraint on this table
-	SELECT conname, class.oid, conkey INTO cname, coid, idarray
-		FROM	pg_constraint AS constr
-			--join with TABLE which the contraint is ON
-			join pg_class AS class ON (constr.conrelid = class.oid)	
-		WHERE class.relname=tabname AND contype='p';
-
-	counter = array_lower(idarray, 1);
-		
-	WHILE (counter <= array_upper(idarray, 1)) LOOP
-		--selecting columns
-		SELECT attname INTO currname
-		FROM pg_attribute AS att
-		WHERE att.attrelid = coid AND att.attnum = idarray[counter];
-		r = ROW(cname, currname, NULL, NULL);
-		RETURN NEXT r;
-		counter = counter + 1;
-	END LOOP;
-
 	--select foreign key constraints on this table
 	--constraint name, column names, oid of referenced table, name of referenced table
-	FOR cname, idarray, refidarray, refcoid, refname IN SELECT conname, constr.conkey, constr.confkey, class2.oid, class2.relname
-			FROM	pg_constraint AS constr
+	FOR cname, idarray, refidarray, coid, refcoid, refname IN 
+		SELECT conname, constr.conkey, constr.confkey, class1.oid, class2.oid, class2.relname
+		FROM	pg_constraint AS constr
 			--join with TABLE which the contraint is ON
 			join pg_class AS class1 ON (constr.conrelid = class1.oid)	
 			--join with referenced TABLE
 			join pg_class AS class2 ON (constr.confrelid = class2.oid)
-			WHERE class1.relname=tabname AND contype='f'					
+		WHERE class1.relname=tabname AND contype='f'					
 	LOOP			
 		counter = array_lower(idarray, 1);
 		
@@ -82,24 +115,115 @@ END
 $$
 LANGUAGE plpgsql;
 
-drop table deska.test;
+--SELECT fk_constraints_on_table('test');
 
-create table deska.test(
-	uid int,
-	uid2 int,	
-	ruid int,
-	ruid2 int,
-	constraint pk_test primary key (uid,uid2),
-	constraint fk_test foreign key (ruid,ruid2) references reftest (uid,uid2)
+
+
+CREATE TYPE c_constraints_on_table_type AS(
+conname	name,
+attname	name,
+condition text
 );
 
-drop table deska.reftest;
+--DROP FUNCTION c_constraints_on_table(name);
 
-create table deska.reftest(
-	uid int,
-	uid2 int,
-	constraint pk_reftest primary key (uid,uid2)
+CREATE OR REPLACE FUNCTION c_constraints_on_table(tabname name)
+RETURNS SETOF c_constraints_on_table_type
+AS
+$$
+DECLARE
+	--contraint name
+	cname name;
+	--name of table that is referenced from this table
+	counter int;
+	--name of currently processeding column name
+	currname name;
+	--list of column ids
+	idarray smallint[];
+	--class oid of table
+	coid int;
+	--one row of result (constraint_name, columns_names, referenced_table_name, referenced_columns_names)
+	r c_constraints_on_table_type;
+	--text of check constraint = consrc
+	constrtext text;
+BEGIN
+	--select check constraints on this table
+	--constraint name, column names, oid of referenced table, name of referenced table
+	FOR cname, coid, idarray, constrtext IN SELECT constr.conname, class.oid, constr.conkey, constr.consrc
+		FROM	pg_constraint AS constr
+			--join with TABLE which the contraint is ON
+			join pg_class AS class ON (constr.conrelid = class.oid)	
+		WHERE class.relname=tabname AND constr.contype='c'					
+	LOOP			
+		counter = array_lower(idarray, 1);
+		
+		WHILE (counter <= array_upper(idarray, 1)) LOOP
+			--selecting columns
+			SELECT attname INTO currname
+			FROM pg_attribute AS att
+			WHERE att.attrelid = coid AND att.attnum = idarray[counter];
+		
+			r = ROW(cname, currname, constrtext);
+			RETURN NEXT r;
+			counter = counter + 1;
+		END LOOP;
+	END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
+--SELECT c_constraints_on_table('test');
+
+
+
+CREATE TYPE u_constraints_on_table_type AS(
+conname	name,
+attname	name
 );
 
+DROP FUNCTION u_constraints_on_table(name);
 
-select constraints_on_table('test'); 
+CREATE OR REPLACE FUNCTION u_constraints_on_table(tabname name)
+RETURNS SETOF u_constraints_on_table_type
+AS
+$$
+DECLARE
+	--contraint name
+	cname name;
+	--counter of pk columns
+	counter int;
+	--name of currently processeding column name
+	currname name;
+	--list of column ids
+	idarray smallint[];
+	--class oid of table
+	coid int;
+	--one row of result (constraint_name, columns_names, referenced_table_name, referenced_columns_names)
+	r u_constraints_on_table_type;
+BEGIN
+	--select primary key constraint on this table
+	FOR cname, coid, idarray IN SELECT constr.conname, class.oid, constr.conkey
+	FROM	pg_constraint AS constr
+		--join with TABLE which the contraint is ON
+		join pg_class AS class ON (constr.conrelid = class.oid)	
+	WHERE class.relname=tabname AND constr.contype='u'
+	LOOP
+		counter = array_lower(idarray, 1);
+		
+		WHILE (counter <= array_upper(idarray, 1)) LOOP
+			--selecting columns
+			SELECT attname INTO currname
+			FROM pg_attribute AS att
+			WHERE att.attrelid = coid AND att.attnum = idarray[counter];
+			r = ROW(cname, currname);
+			RETURN NEXT r;
+			counter = counter + 1;
+		END LOOP;
+	END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
+--SELECT u_constraints_on_table('test');
+
+
