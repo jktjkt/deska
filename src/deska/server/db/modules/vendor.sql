7a64693679a6f43dc9cf805f57dbc5f45c884c2d
Lukáš Kerpl <luke7@seznam.cz>
2011-03-03 09:38:30 +0100

    update vendor module to test prev commit

--- src/deska/server/db/modules/vendor.sql
+++ src/deska/server/db/modules/vendor.sql
@@ -5,9 +5,12 @@
 --
 SET search_path TO production;
 
+CREATE SEQUENCE vendor_uid START 1;
+
 -- vendors of hw
 CREATE TABLE vendor (
-	uid bigint
+	-- nextval can be used - will aplly to history table
+	uid bigint default nextval('vendor_uid')
 		constraint vendor_pk PRIMARY KEY,
 	-- this column is required in all plugins
 	name text NOT NULL
