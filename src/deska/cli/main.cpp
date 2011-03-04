d5cc670064547253d04a6c3e0d3b227fe576eccd
Jan Kundrát <kundratj@fzu.cz>
2011-03-04 18:13:22 +0100

    Remove tests from Parser's main.cpp now that the unit tests are in place
    Refs #96.

--- src/deska/cli/main.cpp
+++ src/deska/cli/main.cpp
@@ -24,46 +24,6 @@ int main()
 
     Deska::CLI::Parser parser( fake );
 
-    std::string test;
-
-    // unit test: error_in_datatype_of_first_inline
-    test = "hardware abcde id xx name \"jmeno\" price 1234.5";
-    parser.parseLine( test );
-    parser.clearContextStack();
-
-    std::cout << std::endl;
-
-    // unit test: error_in_first_attr_name_inline
-    test = "hardware abcde isd 123 name \"jmeno\" price 1234.5";
-    parser.parseLine( test );
-    parser.clearContextStack();
-
-    std::cout << std::endl;
-
-    // unit test: error_toplevel_name
-    test = "haware abcde id 123 name \"jmeno\" price 1234.5";
-    parser.parseLine( test );
-    parser.clearContextStack();
-
-    std::cout << std::endl;
-
-    // unit test: nested_interface
-    test = "host abcde";
-    parser.parseLine( test );
-    test = "name \"as123\"";
-    parser.parseLine( test );
-    test = "interface eth0";
-    parser.parseLine( test );
-    test = "mac \"nejakamac\"";
-    parser.parseLine( test );
-    parser.clearContextStack();
-
-    std::cout << std::endl;
-    
-    test = "hardware abcde id 123 name \"jmeno\" interface eth0 mac \"nejakamac\" price 1234.5";
-    parser.parseLine( test );
-    parser.clearContextStack();
-
     /*
     while ( getline( std::cin, str ) ) {
         if ( str.empty() || str[ 0 ] == 'q' || str[ 0 ] == 'Q' )
