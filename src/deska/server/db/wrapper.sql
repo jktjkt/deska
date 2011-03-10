7e760b3d84d022fcd4f9bd3e7ed49963208d2eb9
Lukáš Kerpl <luke7@seznam.cz>
2011-03-10 16:50:16 +0100

    wrapper now works in db
    Refs #123

---
+++ src/deska/server/db/wrapper.sql
@@ -0,0 +1,89 @@
+BEGIN;
+
+SET search_path TO genproc,history,production,deska;
+
+CREATE FUNCTION getfn(fntype text, kindname text, attributename text = '')
+RETURNS text
+AS
+$$
+# strings
+fn3_string = "{0}_{1}_{2}"
+fn_string = "{0}_{1}"
+
+
+if (fntype == "set") or  (fntype == "rem") :
+	fname = fn3_string.format(kindname,fntype,attributename)
+else:
+	fname = fn_string.format(kindname,fntype)
+return fname
+$$
+LANGUAGE plpythonu;
+
+
+CREATE FUNCTION setAttribute(kindname text, objectname text, attributename text, value text)
+RETURNS text
+AS
+$$
+plan = plpy.prepare("SELECT * from getfn('set',$1,$2)", [ "text", "text"])
+res = plpy.execute(plan, [ kindname, attributename], 1)
+fname = res[0]["getfn"]
+plan = plpy.prepare("SELECT * from " + fname + "($1,$2)", [ "text", "text"])
+res = plpy.execute(plan, [ objectname, value], 1)
+return res[0][fname]
+$$
+LANGUAGE plpythonu;
+
+CREATE FUNCTION removeAttribute(kindname text, objectname text, attributename text)
+RETURNS text
+AS
+$$
+plan = plpy.prepare("SELECT * from getfn('rem',$1,$2)", [ "text", "text"])
+res = plpy.execute(plan, [ kindname, attributename], 1)
+fname = res[0]["getfn"]
+plan = plpy.prepare("SELECT * from " + fname + "($1)", [ "text"])
+res = plpy.execute(plan, [ objectname], 1)
+return res[0][fname]
+$$
+LANGUAGE plpythonu;
+
+CREATE FUNCTION changeObjectName(kindname text, oldname text, newname text)
+RETURNS text
+AS
+$$
+plan = plpy.prepare("SELECT * from getfn('set',$1,$2)", [ "text", "text"])
+res = plpy.execute(plan, [ kindname, "name"], 1)
+fname = res[0]["getfn"]
+plan = plpy.prepare("SELECT * from " + fname + "($1)", [ "text", "text"])
+res = plpy.execute(plan, [ objectname], 1)
+return res[0][fname]
+$$
+LANGUAGE plpythonu;
+
+CREATE FUNCTION createObject(kindname text, objectname text)
+RETURNS text
+AS
+$$
+plan = plpy.prepare("SELECT * from getfn('add',$1)", [ "text"])
+res = plpy.execute(plan, [ kindname], 1)
+fname = res[0]["getfn"]
+plan = plpy.prepare("SELECT * from " + fname + "($1)", [ "text"])
+res = plpy.execute(plan, [ objectname], 1)
+return res[0][fname]
+$$
+LANGUAGE plpythonu;
+
+CREATE FUNCTION deleteObject(kindname text, objectname text)
+RETURNS text
+AS
+$$
+plan = plpy.prepare("SELECT * from getfn('del',$1)", [ "text"])
+res = plpy.execute(plan, [ kindname], 1)
+fname = res[0]["getfn"]
+plan = plpy.prepare("SELECT * from " + fname + "($1)", [ "text"])
+res = plpy.execute(plan, [ objectname], 1)
+return res[0][fname]
+$$
+LANGUAGE plpythonu;
+
+
+END;
