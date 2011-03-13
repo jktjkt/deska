d99002bbd0bf0f953736646ab07217835c17fc48
Lukáš Kerpl <luke7@seznam.cz>
2011-03-13 16:47:35 +0100

    Use new commit function

--- src/deska/server/db/api_usage.sql
+++ src/deska/server/db/api_usage.sql
@@ -15,11 +15,8 @@ SELECT setAttribute('hardware','hwDELL','warranty','10.1.2013');
 SELECT setAttribute('hardware','hwDELL','note','cesky text v repozitari');
 
 -- commit
-SELECT vendor_commit();
-SELECT hardware_commit();
-
 -- end session, part of vendor commit? of vendor commit part of this?
-SELECT version_commit();
+SELECT commit();
 
 SELECT * FROM vendor_history;
 SELECT * FROM hardware_history;
@@ -31,9 +28,7 @@ SELECT deleteObject('hardware','hwDELL');
 SELECT deleteObject('vendor','DELL');
 SELECT createObject('vendor','HP');
 SELECT createObject('vendor','IBM');
-SELECT hardware_commit();
-SELECT vendor_commit();
-SELECT version_commit();
+SELECT commit();
 
 SELECT * FROM vendor_history;
 SELECT * FROM vendor;
