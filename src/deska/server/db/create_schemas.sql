64bc6697160797b4c468b39eb787c09b931255c8
Lukáš Kerpl <luke7@seznam.cz>
2011-03-17 09:37:59 +0100

    New schema api added
    Refs #131

--- src/deska/server/db/create_schemas.sql
+++ src/deska/server/db/create_schemas.sql
@@ -1,24 +1,34 @@
+-- admin and user role
+CREATE ROLE deska_admin;
+CREATE ROLE deska_user;
+
+
 -- what schemas we are using
 
 --
 -- schema for production tables
 -- here place the modules
 --
-CREATE SCHEMA production AUTHORIZATION deska_team;
+CREATE SCHEMA production AUTHORIZATION deska_admin;
 
 --
 -- schema for history tables
 --
-CREATE SCHEMA history AUTHORIZATION deska_team;
+CREATE SCHEMA history AUTHORIZATION deska_admin;
 
 --
 -- schema for generated procedures
 -- this is where generated things are placed
 --
-CREATE SCHEMA genproc AUTHORIZATION deska_team;
+CREATE SCHEMA genproc AUTHORIZATION deska_admin;
 
 --
 -- schema for other functions
 -- here should be all hand writed functions, views, types etc.
 --
-CREATE SCHEMA deska AUTHORIZATION deska_team;
+CREATE SCHEMA deska AUTHORIZATION deska_admin;
+
+--
+-- schema for api functions
+--
+CREATE SCHEMA api AUTHORIZATION deska_user;
