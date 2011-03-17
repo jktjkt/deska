825cfb6512466c2a68a2d931ca5b7c3e8e969db3
Martina Krejcova <martinka.krejcova@seznam.cz>
2011-03-17 11:01:19 +0000

    data_generator - vendor, hardware

--- src/deska/server/db/data_generator/generator.py
+++ src/deska/server/db/data_generator/generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 import random, datetime
-from randdom import Names, Macs, IPv4s 
+from randdom import Names, Macs, IPv4s, Dates
 
 script_template = '''SET search_path TO genproc,history,deska,production;
 BEGIN;
@@ -14,12 +14,6 @@ SELECT commit();
 ROLLBACK;
  '''
  
-hardware_add_template = '''SELECT hardware_add('{hardware}');
-'''
-hardware_set_template = '''SELECT hardware_set_vendor('{hardware}','{vendor}');
-SELECT hardware_set_purchase('{hardware}','{purchase}');
-SELECT hardware_set_warranty('{hardware}','{warranty}');
-'''
 host_add_template = '''SELECT host_add('{host}');
 '''
 host_set_template = '''SELECT host_set_hardware('{host}','{hardware}');
@@ -35,25 +29,51 @@ SELECT interface_set_mac('{interface}','{mac}');
 class Generator():
 
 	vendor_add_template = "SELECT vendor_add('{0}');"
-	vendor = set()
-	hardware = set()
-	
-	def __init__(self, _count = 2):
-		self.count = _count
+	hardware_add_template = "SELECT hardware_add('{0}');"
+	hardware_set_template = '''SELECT hardware_set_vendor('{0}','{1}');
+SELECT hardware_set_purchase('{0}','{2}');
+SELECT hardware_set_warranty('{0}','{3}');
+'''
+
+	def __init__(self, count = 2):
+		self.count = count
 		self.data = list()
 
-	def add_vendors(self,c = 0):
-		if (c is 0):			
-			c = self.count
+	def add_vendors(self,count = 0):
+		if (count is 0):			
+			count = self.count
 		names = Names("names.txt")
 		# gen set of N random (and unique) names
-		self.vendor = names.rset(c) 
+		self.vendor = names.rset(count) 
 		str = map(self.vendor_add_template.format, self.vendor)
 		self.data.extend(str)
+		
+	def add_hardwares(self,count = 0):
+		if (count is 0):			
+			count = self.count * 3
+		names = Names("names.txt")
+		# gen set of N random (and unique) names
+		self.hardware = names.rset(count) 
+		str = map(self.hardware_add_template.format, self.hardware)
+		self.data.extend(str)
+		
+		#set part of hardware
+		purchase_dates = Dates()
+		# get list (not unique) of N dates
+		purchase = purchase_dates.rlist(count)
+		warranty_dates = Dates(2011,4)
+		# get list (not unique) of N dates
+		warranty = warranty_dates.rlist(count)
+		str = map(self.hardware_set_template.format, self.hardware, self.vendor, purchase, warranty)
+		self.data.extend(str)
+		
+	
+	
 
 
-generator = Generator(5)
+generator = Generator(2)
 generator.add_vendors()
+generator.add_hardwares()
 
 # debug
 print generator.data
