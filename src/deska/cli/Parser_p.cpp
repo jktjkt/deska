96a831519b0f8bdc0247e28658e3651ed3bd3fea
Jan Kundrát <kundratj@fzu.cz>
2011-03-18 09:35:53 +0100

    Adding Tomas as an author to files to which he significantly contributed

--- src/deska/cli/Parser_p.cpp
+++ src/deska/cli/Parser_p.cpp
@@ -1,4 +1,6 @@
-/* Copyright (C) 2011 Jan Kundrát <kundratj@fzu.cz>
+/*
+* Copyright (C) 2011 Jan Kundrát <kundratj@fzu.cz>
+* Copyright (C) 2011 Tomáš Hubík <hubik.tomas@gmail.com>
 *
 * This file is part of the Deska, a tool for central administration of a grid site
 * http://projects.flaska.net/projects/show/deska
