55b8d63e72b625bd6a9f414b125aeaad5969b41f
Jan Kundrát <kundratj@fzu.cz>
2011-04-01 19:59:50 +0200

    Let's perform real interaction
    This is still pretty much untested, but should be roughly how we're gonna do
    this later.
    
    refs #142

--- src/deska/cli/main.cpp
+++ src/deska/cli/main.cpp
@@ -1,32 +1,14 @@
 #include <iostream>
 #include <string>
-#include "deska/db/FakeApi.h"
+#include "deska/db/Connection.h"
 #include "CliInteraction.h"
 #include "Parser.h"
 
 int main()
 {
-    using namespace Deska::Db;
-    using namespace Deska::Cli;
-
-    FakeApi *fake = new FakeApi();
-
-    fake->attrs["hardware"].push_back( KindAttributeDataType( "id", TYPE_INT ) );
-    fake->attrs["hardware"].push_back( KindAttributeDataType( "name", TYPE_STRING ) );
-    fake->attrs["hardware"].push_back( KindAttributeDataType( "price", TYPE_DOUBLE ) );
-    fake->attrs["interface"].push_back( KindAttributeDataType( "ip", TYPE_STRING ) );
-    fake->attrs["interface"].push_back( KindAttributeDataType( "mac", TYPE_STRING ) );
-    fake->attrs["host"].push_back( KindAttributeDataType( "hardware_id", TYPE_IDENTIFIER ) );
-    fake->attrs["host"].push_back( KindAttributeDataType( "name", TYPE_STRING ) );
-
-    fake->relations["interface"].push_back( ObjectRelation::embedInto("host") );
-
-    Parser parser( fake );
-
-    CliInteraction cli(fake, &parser);
+    Deska::Db::Connection conn;
+    Deska::Cli::Parser parser(&conn);
+    Deska::Cli::CliInteraction cli(&conn, &parser);
     cli.run();
-
-    delete fake;
-
     return 0;
 }
