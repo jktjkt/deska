ce7a7092b4b2e113e886df1ee0e265205e63c5b8
Jan Kundrát <kundratj@fzu.cz>
2011-04-28 15:40:56 +0200

    Publish build results at the hosted dashboard

---
+++ CTestConfig.cmake
@@ -0,0 +1,5 @@
+set(CTEST_NIGHTLY_START_TIME "02:00:00 CET")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Deska")
+set(CTEST_DROP_SITE_CDASH TRUE)
