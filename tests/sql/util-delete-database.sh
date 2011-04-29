2d2f9fc937ce675482bb28ce054b2c85924ad6f4
Jan Kundrát <kundratj@fzu.cz>
2011-04-29 22:35:18 +0200

    Delete DB and only then delete the user to prevent dangling object ownership

--- tests/sql/util-delete-database.sh
+++ tests/sql/util-delete-database.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 
 . ./util-config.sh
 
-psql -q -U $SU -c "DROP USER IF EXISTS ${USER};" || die "Drop user"
 psql -q -U $SU -c "DROP DATABASE IF EXISTS ${DB};" || die "Drop database"
+psql -q -U $SU -c "DROP USER IF EXISTS ${USER};" || die "Drop user"
