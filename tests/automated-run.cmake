6980751dd1a86b4089f0211f03219c94b6deaefc
Jan Kundrát <kundratj@fzu.cz>
2011-04-29 16:39:37 +0200

    Bring the automated test script up to speed

--- tests/automated-run.cmake
+++ tests/automated-run.cmake
@@ -4,8 +4,8 @@
 SET($ENV{LC_MESSAGES} "en_EN")
 SET(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 
-SET(CTEST_SOURCE_DIRECTORY "/home/jkt/work/fzu/tests-deska")
-SET(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/_build")
+SET(CTEST_SOURCE_DIRECTORY "path-to-/deska_src")
+SET(CTEST_BINARY_DIRECTORY "path-to-/deska_build")
 
 set(CTEST_PROJECT_NAME "Deska")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
@@ -15,7 +15,7 @@ set(WITH_COVERAGE TRUE)
 
 #######################################################################
 
-#ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 
 find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
@@ -31,6 +31,10 @@ set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 
 set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DWITH_TESTING:BOOL=ON ${CTEST_BUILD_OPTIONS}")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
+# Running the SQL tests currently requires PgSQL roles to be set up. You're
+# supposed to `cd tests/sql/; ./util-create-shared-roles.sh` exactly once to
+# create the deska_admin and deska_user roles.
+set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DRUN_SQL_TESTS=1")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
 
 ctest_start("Nightly")
