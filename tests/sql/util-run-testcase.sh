0665a8a4575e3ffd9ecd5ca8b36160df2265333c
Jan Kundrát <kundratj@fzu.cz>
2011-04-29 14:54:50 +0200

    Adding an SQL test runner
    refs #216

---
+++ tests/sql/util-run-testcase.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+cd `dirname $0`
+
+. ./util-config.sh
+
+if [[ -z $1 ]]; then
+    die "No test case to run. Execution"
+fi
+
+# Always remove stuff, so that a possible error in previous test does not leave
+# stuff broken
+./util-delete-database.sh 2>/dev/null
+
+./util-create-database.sh || die "Cannot prepare the DB environment"
+
+# FIXME: setup deska here
+
+psql -q -U $USER -d $DB -v ON_ERROR_STOP=1 -f $1 || die "SQL error"
+
+./util-delete-database.sh || die "Cannot remove the DB after the test"
