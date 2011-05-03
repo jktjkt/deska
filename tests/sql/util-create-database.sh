453910ba75674b2fdf9eae2da83a0d70a126a81e
Jan Kundrát <kundratj@fzu.cz>
2011-05-03 15:30:19 +0200

    Unit tests should use PgPython as their stored procedures

--- tests/sql/util-create-database.sh
+++ tests/sql/util-create-database.sh
@@ -9,4 +9,20 @@ for role in deska_admin deska_user; do
     psql -q -U $SU -c "GRANT ${role} TO ${USER};" || die "Grant ${role}"
 done
 psql -q -U $SU -c "CREATE DATABASE ${DB} OWNER deska_admin;" || die "Create database"
-psql -q -U $SU -d $DB -c "CREATE LANGUAGE plpythonu;" || die "Create language"
+
+psql -q -U $SU -d $DB -c "BEGIN;
+CREATE SCHEMA __python__;
+SET search_path TO __python__;
+CREATE FUNCTION handler() RETURNS LANGUAGE_HANDLER
+ LANGUAGE C AS 'python', 'pl_handler';
+CREATE FUNCTION validator(oid) RETURNS VOID
+ LANGUAGE C AS 'python', 'pl_validator';
+COMMIT;
+
+BEGIN;
+SET search_path TO __python__;
+CREATE FUNCTION inline(INTERNAL) RETURNS VOID
+ LANGUAGE C AS 'python', 'pl_inline';
+CREATE LANGUAGE python
+ HANDLER handler INLINE inline VALIDATOR validator;
+COMMIT;" || die "Enabling PgPython"
