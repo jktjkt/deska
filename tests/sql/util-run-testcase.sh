d39d9841c6993b7956a5070ba3a65b36c6a08380
Jan Kundrát <kundratj@fzu.cz>
2011-05-04 18:25:50 +0200

    Take care to check the exit status from deska_install.sh
    I'm to be blamed for this one.

--- tests/sql/util-run-testcase.sh
+++ tests/sql/util-run-testcase.sh
@@ -27,7 +27,7 @@ fi
 psql -q -U $DESKA_USER -d $DESKA_DB -f $PGTAP_FILE || die "Installing pgTAP"
 
 pushd ../../src/deska/server/db
-./deska_install.sh install -u $DESKA_SU -d $DESKA_DB --all
+./deska_install.sh install -u $DESKA_SU -d $DESKA_DB --all || die "Running deska_install"
 popd
 
 #psql -q -U $DESKA_USER -d $DESKA_DB -v ON_ERROR_STOP=1 -f $1 || die "SQL exection"
