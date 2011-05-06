57fedc7f7c28adbf96661539b8532461aff17302
Jan Kundrát <kundratj@fzu.cz>
2011-05-06 12:26:29 +0200

    Adjust the testcase wrapper to always respect cmake settings
    You're now supposed to configure with -DRUN_SQL_TESTS=1 (which enables SQL
    integration at all) and control whether or not to skip full drop of a DB by
    -DDESKA_SQL_TEST_FAST_BUT_NONDETERMINISTIC. Standard CMake rules for boolean
    variables apply here.

--- tests/sql/util-run-testcase.sh
+++ tests/sql/util-run-testcase.sh
@@ -12,21 +12,26 @@ if [[ ! -f $1 ]]; then
     die "Locating SQL testcase"
 fi
 
-# Always remove stuff, so that a possible error in previous test does not leave
-# stuff broken
-if [[ -z `find . -atime -1 -name .db_installed` ]]; then
+DESKA_DB_STATE_FILE=./.db_initialized
+
+if [[ -n "${DESKA_SQL_TEST_FAST_BUT_NONDETERMINISTIC}" ]] && [[ -f $DESKA_DB_STATE_FILE ]]; then
+    # Use the fast way of doing things
+	pushd "${DESKA_SOURCES}/src/deska/server/db"
+	./deska_install.sh restore --all -u $DESKA_SU -d $DESKA_DB || die "Running deska_install"
+	popd
+else
+    # Use the slower way which is completely deterministic
+
+    # Always remove stuff, so that a possible error in previous test does not leave
+    # stuff broken
 	./util-delete-database.sh 2>/dev/null
+
 	./util-create-database.sh || die "Preparing the DB environment"
 
 	pushd "${DESKA_SOURCES}/src/deska/server/db"
 	./deska_install.sh install -u $DESKA_SU -d $DESKA_DB --all || die "Running deska_install"
 	popd
-	touch .db_installed
-else
-	# clean db is enough
-	pushd "${DESKA_SOURCES}/src/deska/server/db"
-	./deska_install.sh restore --all -u $DESKA_SU -d $DESKA_DB || die "Running deska_install"
-	popd
+	touch $DESKA_DB_STATE_FILE
 fi
 
 pg_prove -U $DESKA_SU -d $DESKA_DB $1 || die "Test"
