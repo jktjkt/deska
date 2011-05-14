92b8efb39f07d86556c100e0ac43c4009698be0f
Tomas Hubik <hubik.tomas@gmail.com>
2011-05-14 22:45:38 +0200

    Use new classes for the CLI

--- src/deska/cli/main.cpp
+++ src/deska/cli/main.cpp
@@ -2,13 +2,17 @@
 #include <string>
 #include "deska/db/Connection.h"
 #include "CliInteraction.h"
+#include "ParserSignals.h"
+#include "UserInterface.h"
 #include "Parser.h"
 
 int main()
 {
     Deska::Db::Connection conn;
     Deska::Cli::Parser parser(&conn);
-    Deska::Cli::CliInteraction cli(&conn, &parser);
-    cli.run();
+    Deska::Cli::CliInteraction db(&conn);
+    Deska::Cli::UserInterface ui(std::cout, std::cerr, std::cin, &db, &parser);
+    Deska::Cli::SignalsHandler(&parser, &ui);
+    ui.run();
     return 0;
 }
