6319e9ef8a93a3b1519a647e9b8f9539dc77015c
Tomas Hubik <hubik.tomas@gmail.com>
2011-05-18 21:22:24 +0200

    Documentation

--- src/deska/db/Objects.h
+++ src/deska/db/Objects.h
@@ -175,11 +175,19 @@ bool operator!=(const ObjectRelation &a, const ObjectRelation &b);
 std::ostream& operator<<(std::ostream &stream, const ObjectRelation &o);
 
 
+/** @short Structure for pairs kind name - object name. */
 struct ObjectDefinition
 {
+    /** @short Constructor only assignes the data members.
+    *
+    *   @param kindName Name of the kind (eg. host)
+    *   @param objectName Name of the instance of the kind (eg. hpv2)
+    */
     ObjectDefinition(const Identifier &kindName, const Identifier &objectName);
 
+    /** Name of the kind */
     Identifier kind;
+    /** Name of the instance of the kind */
     Identifier name;
 };
 
@@ -188,11 +196,19 @@ bool operator==(const ObjectDefinition &a, const ObjectDefinition &b);
 bool operator!=(const ObjectDefinition &a, const ObjectDefinition &b);
 
 
+/** @short Structure for pairs attribute name - attribute value. */
 struct AttributeDefinition
 {
+    /** @short Constructor only assignes the data members.
+    *
+    *   @param attributeName Name of the attribute (eg. ip)
+    *   @param assignedValue Value of the attribute (eg. 192.168.10.56)
+    */
     AttributeDefinition(const Identifier &attributeName, const Value &assignedValue);
 
+    /** Name of the attribute */
     Identifier attribute;
+    /** Value of the attribute */
     Value value;
 };
 
