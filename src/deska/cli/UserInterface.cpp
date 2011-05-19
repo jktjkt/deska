28910187395eb797e435a362e85ef333aec001da
Jan Kundrát <kundratj@fzu.cz>
2011-05-19 13:15:19 +0200

    Remove dead code

--- src/deska/cli/UserInterface.cpp
+++ src/deska/cli/UserInterface.cpp
@@ -298,7 +298,6 @@ void UserInterface::eventLoop()
     std::string line;
     out << prompt;
     Db::ContextStack context;
-    std::ostringstream promptBuilder;
     while (getline(in, line)) {
         // FIXME: For some reason some times the line is read even though user did not enter anything. Bug #222
         // Hack for bug #222.
