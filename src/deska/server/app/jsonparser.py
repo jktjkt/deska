3580f40f2df557ef29f54042b9278bfd8397d782
Lukáš Kerpl <luke7@seznam.cz>
2011-05-20 21:07:43 +0200

    Rewrite deska server for work with json returning db functions

---
+++ src/deska/server/app/jsonparser.py
@@ -0,0 +1,31 @@
+import json
+
+import logging
+LOG_FILENAME = 'deska_server.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+
+CMD = "command"
+ERR = "dbException"
+
+class CommandParser:
+	# dict of commands
+	# create json object from string in data
+	def __init__(self,data):
+		logging.debug("loading data {d}".format(d = data))
+		self.jsn = json.loads(data)
+		# if it does not have CMD, error
+		if CMD not in self.jsn:
+			raise Exception("No CMD in json: " + self.jsn)
+
+	def getfn(self):
+		cmd = self.jsn[CMD]
+		logging.debug("function name: " + cmd)
+		return self.jsn[CMD]
+
+	def getargs(self):
+		args = self.jsn.copy()
+		del args[CMD]
+		logging.debug("arguments are {a}".format(a = args))
+		return args
+		
