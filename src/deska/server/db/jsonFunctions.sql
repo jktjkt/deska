7dfb535d80ffa204ec052994838f5e73c3c8358d
Lukáš Kerpl <luke7@seznam.cz>
2011-05-21 12:41:36 +0200

    Json returng kind functions

---
+++ src/deska/server/db/jsonFunctions.sql
@@ -0,0 +1,120 @@
+SET search_path TO deska,api,genproc,history,versioning,production;
+DROP SCHEMA jsn CASCADE;
+CREATE SCHEMA jsn;
+
+CREATE OR REPLACE FUNCTION jsn.kindNames()
+RETURNS text
+AS
+$$
+import Postgres
+import json
+
+@pytypes
+def main():
+	name = "kindNames"
+	plan = prepare('SELECT * FROM api.kindNames()')
+	cur = plan()
+	
+	res = list()
+	for line in cur:
+		res.append(str(line[0]))
+
+	jsn = dict()
+	jsn["response"] = name
+	jsn[name] = res
+	jsn["response"] = "kindNames"
+	return json.dumps(jsn)
+$$
+LANGUAGE python;
+
+CREATE OR REPLACE FUNCTION jsn.kindAttributes(kindName text)
+RETURNS text
+AS
+$$
+import Postgres
+import json
+
+type_dict = ({
+	"int8": "int",
+	"int4": "int",
+	"text": "string",
+	"bpchar": "string",
+	"date": "string",
+	"macaddr": "string",
+	"inet": "string"
+})
+
+@pytypes
+def main(kindName):
+	name = "kindAttributes"
+	plan = prepare('SELECT * FROM api.kindAttributes($1)')
+	cur = plan(kindName)
+	
+	res = list()
+	for line in cur:
+		typ = dict()
+		typ[str(line[0])] = type_dict[str(line[1])]
+		res.append(typ)
+
+	jsn = dict()
+	jsn["response"] = name
+	jsn[name] = res
+	jsn["kindName"] = kindName
+	return json.dumps(jsn)
+$$
+LANGUAGE python;
+
+CREATE OR REPLACE FUNCTION jsn.kindRelations(kindName text)
+RETURNS text
+AS
+$$
+import Postgres
+import json
+
+@pytypes
+def main(kindName):
+	name = "kindRelations"
+	plan = prepare('SELECT * FROM api.kindRelations($1)')
+	cur = plan(kindName)
+	
+	res = list()
+	for line in cur:
+		rel = dict()
+		# FIXME: hotfix until rewrite relations get functions
+		rel["relation"] = "EMBED_INTO"
+		rel["into"] = str(line[1])
+		res.append(rel)
+
+	jsn = dict()
+	jsn["response"] = name
+	jsn[name] = res
+	jsn["kindName"] = kindName
+	return json.dumps(jsn)
+$$
+LANGUAGE python;
+
+CREATE OR REPLACE FUNCTION jsn.kindInstances(kindName text)
+RETURNS text
+AS
+$$
+import Postgres
+import json
+
+@pytypes
+def main(kindName):
+	name = "kindInstances"
+	plan = prepare('SELECT * FROM api.kindInstances($1)')
+	cur = plan(kindName)
+	
+	res = list()
+	for line in cur:
+		res.append(str(line[0]))
+
+	jsn = dict()
+	jsn["response"] = name
+	jsn[name] = res
+	jsn["kindName"] = kindName
+	return json.dumps(jsn)
+$$
+LANGUAGE python;
+
