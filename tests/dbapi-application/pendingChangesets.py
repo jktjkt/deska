32e1a33ec17a1fa2fb3f61182b62f5364a41a4f8
Jan Kundrát <kundratj@fzu.cz>
2011-05-24 19:04:46 +0200

    More tests for attaching to/detaching from pending chagesets
    Currently failing due to various functions returning extra data, like "1".

--- tests/dbapi-application/pendingChangesets.py
+++ tests/dbapi-application/pendingChangesets.py
@@ -26,5 +26,38 @@ j = [
     {"command": "pendingChangesets"},
     {'response': 'pendingChangesets', "pendingChangesets": []}
     ),
-
+    # FIXME: try to abort once again; that should fail with some DBAPI error
+    # FIXME: also try to detach, now that there's no changeset
+    ( # create new changeset once again
+    {"command": "startChangeset"},
+    {'response': 'startChangeset', "startChangeset": "tmp2"}
+    ),
+    ( # and detach from it
+    {"command":"detachFromCurrentChangeset","message":"xyz"},
+    {'response': 'detachFromCurrentChangeset'}
+    ),
+    ( # verify that it's listed in a correct state
+    {"command": "pendingChangesets"},
+    {'response': 'pendingChangesets', "pendingChangesets": [
+        {'status': 'DETACHED', 'changeset': 'tmp2',
+         'author': apiUtils.DeskaDbUser(),
+         'timestamp': apiUtils.CurrentTimestamp(), 'parentRevision': 'r1',
+         'message': 'xyz'}
+    ]}
+    ),
+    ( # attach to tmp2 again
+    {"command":"resumeChangeset","revision":"tmp2"},
+    {'response': 'resumeChangeset'}
+    ),
+    ( # it should be pending again
+    {"command": "pendingChangesets"},
+    {'response': 'pendingChangesets', "pendingChangesets": [
+        {'status': 'INPROGRESS', 'changeset': 'tmp2',
+         'author': apiUtils.DeskaDbUser(),
+         'timestamp': apiUtils.CurrentTimestamp(), 'parentRevision': 'r1',
+         'message': ''}
+    ]}
+    ),
+    # FIXME: simulate what happens when starting a changeset while being
+    # attached to one
 ]
