a56369824ec4be40576e9522d354f4e05254f86d
Jan Kundrát <kundratj@fzu.cz>
2011-05-24 16:57:40 +0200

    Test that inquiries for the DB scheme are reasonable

--- tests/dbapi-application/dbScheme.py
+++ tests/dbapi-application/dbScheme.py
@@ -2,4 +2,12 @@ j = [(
     {"command": "kindNames"},
     {'response': 'kindNames',
      'kindNames': ['interface', 'vendor', 'host', 'hardware']}
-    )]
+    ),
+    ({"command": "kindAttributes", "kindName": "interface"},
+     {"kindName": "interface", "kindAttributes":
+        {"note": "string", "ip": "string", "host": "int", "mac": "string"},
+      "response": "kindAttributes"}),
+    ({"kindName": "hardware", "command": "kindRelations"},
+     {"kindName": "hardware", "kindRelations": [],
+      "response": "kindRelations"})
+]
