e1b6d924cf04d8a5eb0981611fc19bf01de48ae6
Jan Kundrát <kundratj@fzu.cz>
2011-05-25 12:59:03 +0200

    Test for filtering against TemporaryChangesetId

--- tests/test_json_api_parser.cpp
+++ tests/test_json_api_parser.cpp
@@ -407,6 +407,17 @@ BOOST_FIXTURE_TEST_CASE(json_pendingChangesets, JsonApiTestFixtureFailOnStreamTh
     expectEmpty();
 }
 
+/** @short Test that simple filter for pendingChangesets works */
+BOOST_FIXTURE_TEST_CASE(json_pendingChangesets_filterNe, JsonApiTestFixtureFailOnStreamThrow)
+{
+    expectWrite("{\"command\":\"pendingChangesets\",\"filter\":{\"condition\":\"columnNe\",\"column\":\"revision\",\"value\":\"tmp123\"}}\n");
+    expectRead("{\"response\": \"pendingChangesets\", \"filter\":{\"condition\":\"columnNe\",\"column\":\"revision\",\"value\":\"tmp123\"}, \"pendingChangesets\": []}\n");
+    std::vector<PendingChangeset> expected;
+    std::vector<PendingChangeset> res = j->pendingChangesets(Filter(Expression(FILTER_COLUMN_NE, "revision", TemporaryChangesetId(123))));
+    BOOST_CHECK_EQUAL_COLLECTIONS(res.begin(), res.end(), expected.begin(), expected.end());
+    expectEmpty();
+}
+
 /** @short Basic test for resumeChangeset() */
 BOOST_FIXTURE_TEST_CASE(json_resumeChangeset, JsonApiTestFixtureFailOnStreamThrow)
 {
