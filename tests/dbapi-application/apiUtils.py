jktjkt/deska

commit 5e5b6e0c79e48c6fa0af4f30b03c8870c76be8ff

Simplify the code

tests/dbapi-application/apiUtils.py
@@ -84,14 +84,6 @@ class ApiMethod(object):
     def __repr__(self):
         return str(self.command, self.response)
 
-    def __getitem__(self, i):
-        if i == 0:
-            return self.command
-        elif i == 1:
-            return self.response
-        else:
-            raise IndexError, "Index out of range"
-
 
 # DBAPI commands
 def startChangeset():

tests/dbapi-application/testdbapi.py
@@ -29,7 +29,7 @@ class JsonApiTester(unittest.TestCase):
     def testCommunication(self):
         """Iterate over the recorded communication and verify that we get the same results back"""
         for items in j:
-            json.dump(items[0], self.p.stdin)
+            json.dump(items.command, self.p.stdin)
             self.p.stdin.write("\n")
             self.p.stdin.flush()
             status = select.select((self.p.stdout, self.p.stderr), (), ())
@@ -39,7 +39,7 @@ class JsonApiTester(unittest.TestCase):
             readJson = self.p.stdout.readline()
             print readJson
             output = json.loads(readJson)
-            self.assertEqual(deunicodeify(output), items[1])
+            self.assertEqual(deunicodeify(output), items.response)
 
 if __name__ == "__main__":
     # usage: testdbapi.py /path/to/deska_server.py dbname testcase

commit b4af48c8309eaf7b99ed9b01e3caa728990697a9

Port the rest of the DBAPI unit tests
This fixes a regression I introduced in 5e5b6e0c79e48c6fa0af4f30b03c8870c76be8ff

tests/dbapi-application/apiUtils.py
@@ -86,6 +86,15 @@ class ApiMethod(object):
 
 
 # DBAPI commands
+def kindNames():
+    return ApiMethod("kindNames", None)
+
+def kindAttributes(kindName):
+    return ApiMethod("kindAttributes", {"kindName": kindName})
+
+def kindRelations(kindName):
+    return ApiMethod("kindRelations", {"kindName": kindName})
+
 def startChangeset():
     return ApiMethod("startChangeset", None)
 

tests/dbapi-application/dbScheme.py
@@ -1,15 +1,9 @@
 from apiUtils import *
 
-j = [(
-    {"command": "kindNames"},
-    {'response': 'kindNames',
-     'kindNames': AnyOrderList(('interface', 'vendor', 'host', 'hardware'))}
-    ),
-    ({"command": "kindAttributes", "kindName": "interface"},
-     {"kindName": "interface", "kindAttributes":
-        {"note": "string", "ip": "string", "host": "int", "mac": "string"},
-      "response": "kindAttributes"}),
-    ({"kindName": "hardware", "command": "kindRelations"},
-     {"kindName": "hardware", "kindRelations": [],
-      "response": "kindRelations"})
+j = [
+    kindNames().returns(
+        AnyOrderList(('interface', 'vendor', 'host', 'hardware'))),
+    kindAttributes("interface").returns(
+        {"note": "string", "ip": "string", "host": "int", "mac": "string"}),
+    kindRelations("hardware").returns([])
 ]