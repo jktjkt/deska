jktjkt/deska

commit 12698c156dfa17858dbe7289dba0a70f8b7a60ec

Add better error reporting in dutil file

src/deska/server/db/jsonVersioning.sql
@@ -123,8 +123,12 @@ def main(tag,filter):
 	name = "pendingChangesets"
 	jsn = dutil.jsn(name,tag)
 
-	filter = dutil.Filter(filter)
-	select = "SELECT id2changeset(metadata.id),metadata.author,metadata.status,num2revision(id2num(metadata.parentRevision)),metadata.timestamp,metadata.message FROM changeset AS metadata " + filter.getJoin("metadata") + filter.getWhere() + "ORDER BY metadata.id"
+	try:
+		filter = dutil.Filter(filter)
+		select = "SELECT id2changeset(metadata.id),metadata.author,metadata.status,num2revision(id2num(metadata.parentRevision)),metadata.timestamp,metadata.message FROM changeset AS metadata " + filter.getJoin("metadata") + filter.getWhere() + " ORDER BY metadata.id"
+	except dutil.DutilException as err:
+		return err.json(name,jsn)
+	return select
 	try:
 		colnames,data = dutil.getdata(select)
 	except dutil.DeskaException as err:

src/deska/server/db/dutil.py
@@ -11,7 +11,6 @@ class DeskaException(Exception):
 		'70002': 'ChangesetAlreadyOpenError',
 		'70003': 'NoChangesetError',
 		'70010': 'ReCreateObjectError',
-		'70020': 'FilterError',
 		'70021': 'NotFoundError',
 		'*': 'ServerError'
 	}
@@ -50,6 +49,13 @@ class DeskaException(Exception):
 		jsn["response"] = command
 
 		return json.dumps(jsn)
+
+class DutilException(DeskaException):
+	'''Exception in pgpython code'''
+
+	def __init__(self,type,message):
+		self.type = type
+		self.message = message
 
 def jsn(name,tag):
 	'''Create json sceleton'''
@@ -128,8 +134,7 @@ class Condition():
 			self.parse()
 			return
 		except:
-			pass # do not raise here
-		Postgres.ERROR("Syntax error in condition",code = 70020)
+			raise DutilException("FilterError","Syntax error in condition.")
 	
 	def parse(self):
 		'''Update condition data for easy creation of Deska SQL condition'''
@@ -165,11 +170,9 @@ class Filter():
 			return
 		try:
 			self.data = json.loads(filterData)
-			self.where = self.parse(self.data)
-			return
 		except Exception as err:
-			pass # do not raise another exception in except part
-		Postgres.ERROR("Syntax error when parsing filterData.",code = 70020)
+			raise DutilException("FilterError","Syntax error in filter.")
+		self.where = self.parse(self.data)
 		
 	def getWhere(self):
 		'''Return where part of sql statement'''
@@ -193,7 +196,7 @@ class Filter():
 		'''Parse filter data and create SQL WHERE part'''
 		if self.data == '':
 			return ''
-		try:
+		if "operator" in data:
 			operator = data["operator"]
 			if operator == "and":
 				res = [self.parse(expresion) for expresion in data["operands"]]
@@ -202,9 +205,7 @@ class Filter():
 				res = [self.parse(expresion) for expresion in data["operands"]]
 				return "(" + ") OR (".join(res) + ")"
 			else:
-				Postgres.ERROR("Syntax error: bad operands",code = 70020)
-		except:
-			pass
+				raise DutilException("FilterError","Bad operands.")
 		cond = Condition(data)
 		# collect affected kinds (need for join)
 		self.kinds.add(cond.getAffectedKind())

commit 91ddafbca0d783c92464843ff9e6d03749b6abbb

Fix failing unit tests
This was introduced in 12698c156dfa17858dbe7289dba0a70f8b7a60ec, and broke some
of the unit tests (like the test_dbapi_createObjects).

src/deska/server/db/jsonVersioning.sql
@@ -128,7 +128,6 @@ def main(tag,filter):
 		select = "SELECT id2changeset(metadata.id),metadata.author,metadata.status,num2revision(id2num(metadata.parentRevision)),metadata.timestamp,metadata.message FROM changeset AS metadata " + filter.getJoin("metadata") + filter.getWhere() + " ORDER BY metadata.id"
 	except dutil.DutilException as err:
 		return err.json(name,jsn)
-	return select
 	try:
 		colnames,data = dutil.getdata(select)
 	except dutil.DeskaException as err: