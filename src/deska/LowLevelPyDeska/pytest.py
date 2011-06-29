bb9170dd95cdb678437dcf05d890082974fd0a07
Jan Kundrát <kundratj@fzu.cz>
2011-06-29 13:18:46 +0200

    The code has moved to a dedicated unit test

--- src/deska/LowLevelPyDeska/pytest.py
+++ src/deska/LowLevelPyDeska/pytest.py
@@ -4,50 +4,6 @@ import datetime
 sys.path.append(os.getcwd())
 import libLowLevelPyDeska as _l
 
-def verify(x):
-    print "%s: %s" % (type(x), x)
-    deska_val = _l.Py_2_DeskaDbValue(x)
-    try:
-        non_opt = _l.DeskaDbValue_2_DeskaDbNonOptionalValue(deska_val)
-        print "Deska::Db::NonOptionalValue: str  %s" % str(non_opt)
-        print "Deska::Db::NonOptionalValue: repr %s" % repr(non_opt)
-    except RuntimeError, e:
-        print "deoptionalify failed"
-    py_x = _l.DeskaDbValue_2_Py(deska_val)
-    print "Deska::Db::Value -> Py str : %s" % str(py_x)
-    print "Deska::Db::Value -> Py repr: %s" % repr(py_x)
-    if str(x) != str(py_x):
-        print "*DIFFERENT* strings: %s, %s" % (str(x), str(py_x))
-    if x != py_x:
-        print "*DIFFERENT* objects"
-    print
-
-variants = ("ahoj", 3, 333.666, None,
-            _l.IPv4Address("127.0.0.1"),
-            _l.IPv6Address("::1"),
-            _l.MacAddress("00:16:3e:37:53:2B"),
-            datetime.datetime.now(), datetime.date.today(),
-           )
-
-for x in variants:
-    verify(x)
-
-for x in variants:
-    print "%s: %s" % (type(x), x)
-    metadataval = _l.Py_2_DeskaMetadataValue(x)
-
-    print "Deska::Db::MetadataValue: str  %s" % str(metadataval)
-    print "Deska::Db::MetadataValue: repr %s" % repr(metadataval)
-    py_x = _l.DeskaMetadataValue_2_Py(metadataval)
-    print "Deska::Db::MetadataValue -> Py: str  %s" % str(py_x)
-    print "Deska::Db::MetadataValue -> Py: repr %s" % repr(py_x)
-    if str(x) != str(py_x):
-        print "*DIFFERENT* strings: %s, %s" % (str(x), str(py_x))
-    if x != py_x:
-        print "*DIFFERENT* objects: %s %s, %s %s" % (
-            type(x), x, type(py_x), py_x)
-    print
-
 fe1 = _l.MetadataExpression(
     _l.ComparisonOperator.COLUMN_LT, "revision",
     _l.Py_2_DeskaMetadataValue(_l.RevisionId(333)))
