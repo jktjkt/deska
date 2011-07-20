2a66f6c1eb3c130baab80b5b9cff0bea2b88e142
Jan Kundrát <kundratj@fzu.cz>
2011-07-20 15:44:05 +0200

    Test stream dumping of various filters

--- tests/test_api_filters.cpp
+++ tests/test_api_filters.cpp
@@ -46,13 +46,38 @@ void verifyFilterObject(const std::string &str, const T &value)
 BOOST_AUTO_TEST_CASE(filter_dumping)
 {
     MetadataExpression e1(FILTER_COLUMN_EQ, "revision", RevisionId(123));
+    string s1 = "MetadataExpression(revision == MetadataValue<RevisionId>(r123))";
     MetadataExpression e2(FILTER_COLUMN_NE, "changeset", TemporaryChangesetId(666));
+    string s2 = "MetadataExpression(changeset != MetadataValue<TemporaryChangesetId>(tmp666))";
     MetadataExpression e3(FILTER_COLUMN_LT, "state", PendingChangeset::ATTACH_DETACHED);
+    string s3 = "MetadataExpression(state < MetadataValue<PendingChangeset::AttachStatus>(DETACHED))";
     MetadataExpression e4(FILTER_COLUMN_LE, "author", Value("foo"));
+    string s4 = "MetadataExpression(author <= MetadataValue<Value>(Value<string>(foo)))";
     AttributeExpression e5(FILTER_COLUMN_GT, "kind", "attr", Value(123));
-    verifyFilterObject("MetadataExpression(revision == MetadataValue<RevisionId>(r123))", e1);
-    verifyFilterObject("MetadataExpression(changeset != MetadataValue<TemporaryChangesetId>(tmp666))", e2);
-    verifyFilterObject("MetadataExpression(state < MetadataValue<PendingChangeset::AttachStatus>(DETACHED))", e3);
-    verifyFilterObject("MetadataExpression(author <= MetadataValue<Value>(Value<string>(foo)))", e4);
-    verifyFilterObject("AttributeExpression(kind.attr > Value<int>(123))", e5);
+    string s5 = "AttributeExpression(kind.attr > Value<int>(123))";
+    vector<Filter> v1;
+    v1.push_back(e1);
+    v1.push_back(e2);
+    AndFilter e6(v1);
+    string s6 = "AndFilter([" + s1 + ", " + s2 + ", ])";
+    vector<Filter> v2;
+    v2.push_back(e3);
+    v2.push_back(e4);
+    OrFilter e7(v2);
+    string s7 = "OrFilter([" + s3 + ", " + s4 + ", ])";
+    vector<Filter> v3;
+    v3.push_back(e5);
+    v3.push_back(e6);
+    v3.push_back(e7);
+    AndFilter e8(v3);
+    string s8 = "AndFilter([" + s5 + ", " + s6 + ", " + s7 + ", ])";
+
+    verifyFilterObject(s1, e1);
+    verifyFilterObject(s2, e2);
+    verifyFilterObject(s3, e3);
+    verifyFilterObject(s4, e4);
+    verifyFilterObject(s5, e5);
+    verifyFilterObject(s6, e6);
+    verifyFilterObject(s7, e7);
+    verifyFilterObject(s8, e8);
 }
