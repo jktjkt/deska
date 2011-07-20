ce8f960b4e96a24373313d4c86dee0f32bcd4b12
Jan Kundrát <kundratj@fzu.cz>
2011-07-20 14:38:48 +0200

    Remove obsolete comment
    refs #246

--- tests/dbapi-application/persist_metadata.py
+++ tests/dbapi-application/persist_metadata.py
@@ -19,7 +19,6 @@ def doStuff(r, kinds):
         r.assertEqual(type(r.c(kindRelations(kind))), list)
         r.assertEqual(type(r.c(kindAttributes(kind))), dict)
 
-    # the following tests FAIL, see redmine #246
     r.cfail(kindInstances("error_kind_name"), InvalidKindError())
     r.cfail(kindRelations("error_kind_name"), InvalidKindError())
     r.cfail(kindAttributes("error_kind_name"), InvalidKindError())
