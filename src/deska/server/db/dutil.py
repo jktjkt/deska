jktjkt/deska

commit 81c3c4e1bfe5162f0ed91153358fdbe45f14ea60

The filters should expect "attribute", not "column"

src/deska/server/db/dutil.py
@@ -137,9 +137,9 @@ class Condition():
 				raise DutilException("FilterError","Attribute {0} does not exists.".format(self.col))
 		elif "kind" in data:
 			self.kind = data["kind"]
-			if "column" not in data:
-				raise DutilException("FilterError","Item 'column' is missing in condition.")
-			self.col = data["column"]
+			if "attribute" not in data:
+				raise DutilException("FilterError","Item 'attribute' is missing in condition.")
+			self.col = data["attribute"]
 			if self.kind not in generated.kinds():
 				raise DutilException("FilterError","Kind {0} does not exists.".format(self.col))
 			# add also name 

commit b008695aac945f3a4743c46580f54afc0ce8805d

Fix error caused by 81c3c4e1bfe5162f0ed91153358fdbe45f14ea60

src/deska/server/db/dutil.py
@@ -163,7 +163,7 @@ class Condition():
 			parent, name = fcall("embed_name(text,text)",self.val,'->')
 			self.val = mystr(name)
 			newcond = dict()
-			newcond["column"] = embed[self.kind]
+			newcond["attribute"] = embed[self.kind]
 			newcond["kind"] = self.kind
 			newcond["condition"] = self.op
 			newcond["value"] = mystr(parent)