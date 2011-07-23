9d6cfb858da7442a36a731abfe7dfde391b5bfc1
Jan Kundrát <kundratj@fzu.cz>
2011-07-23 12:49:44 +0200

    Some documentation for the 'deska' module

--- src/deska/LowLevelPyDeska/deska/__init__.py
+++ src/deska/LowLevelPyDeska/deska/__init__.py
@@ -1,3 +1,11 @@
+'''High-level Python bindings for accessing the Deska database
+
+This module provides a convenient way of accessing various objects from the
+Deska database as native Python classes.  A connection to the database is
+constructed according to the default rules (inheriting necessary options from
+the environment and configuration files where applicable); this means that this
+module can only be used when the database is reachable and available.
+'''
 import libLowLevelPyDeska as _l
 from classes import _discoverScheme
 
