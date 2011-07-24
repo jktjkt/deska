7346a81db9db682fe4ca9fd4d3bbca02399ba6d5
Jan Kundrát <kundratj@fzu.cz>
2011-07-24 23:46:51 +0200

    Extra directory for modules/scheme
    This is in preparation of merging the FZU scheme. The end goal is to have an
    environment variable for specifying which scheme to use.

--- install/create_1.sql
+++ install/create_1.sql
@@ -2,11 +2,6 @@ BEGIN;
 -- create schemas
 \i create_schemas_1.sql
 
--- create modules
-\i modules/vendor.sql
-\i modules/hardware.sql
-\i modules/host.sql
-\i modules/interface.sql
---\i modules/large_modul.sql
+-- create-all-modules-here
 
 END;
