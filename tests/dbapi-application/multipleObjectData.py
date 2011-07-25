ab43ae1fa1c89338ffe7d8e88180d3d3e3467a11
Martina Krejcova <martinka.krejcova@seznam.cz>
2011-07-25 12:25:21 +0200

    test multipleObjectData

--- tests/dbapi-application/multipleObjectData.py
+++ tests/dbapi-application/multipleObjectData.py
@@ -3,62 +3,127 @@
 from apiUtils import *
 
 expectedHardwareData = {
-	"hw1": {"vendor": "vendor1", "warranty": "2010-10-10", "purchase": "2008-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
-	"hw2": {"vendor": "vendor1", "warranty": "2010-06-06", "purchase": "2006-06-06", "cpu_num": None, "ram": None, "note": None, "template": None}
+    "hw1": {"vendor": "vendor1", "warranty": "2010-10-10", "purchase": "2008-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
+    "hw2": {"vendor": "vendor1", "warranty": "2010-06-06", "purchase": "2006-06-06", "cpu_num": None, "ram": None, "note": None, "template": None}
 }
 expectedHardwareData2 = {
-	"hw1": {"vendor": "vendor1", "warranty": "2010-10-10", "purchase": "2008-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
-	"hw2": {"vendor": "vendor1", "warranty": "2006-06-06", "purchase": "2010-06-06", "cpu_num": None, "ram": None, "note": None, "template": None},
-	"hw3": {"vendor": "vendor2", "warranty": "2010-10-10", "purchase": "2012-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
-	"hw4": {"vendor": "vendor2", "warranty": "2010-10-10", "purchase": "2012-10-10", "cpu_num": None, "ram": None, "note": None, "template": None}
+    "hw1": {"vendor": "vendor1", "warranty": "2010-10-10", "purchase": "2008-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
+    "hw2": {"vendor": "vendor1", "warranty": "2006-06-06", "purchase": "2010-06-06", "cpu_num": None, "ram": None, "note": None, "template": None},
+    "hw3": {"vendor": "vendor2", "warranty": "2010-10-10", "purchase": "2012-10-10", "cpu_num": None, "ram": None, "note": None, "template": None},
+    "hw4": {"vendor": "vendor2", "warranty": "2010-10-10", "purchase": "2012-10-10", "cpu_num": None, "ram": None, "note": None, "template": None}
+}
+
+expectedInterfaceData = {
+    "host1->eth0": {"note": "some note", "mac": "01:23:45:67:89:aa", "ip6": None, "ip4": "192.168.0.1", "template": None},
+    "host2->eth0": {"note": "another note", "mac": "01:23:45:67:89:bb", "ip6": None, "ip4": None, "template": None}
+}
+
+expectedInterfaceData2 = {
+    "host1->eth0": {"note": "some note", "mac": "01:23:45:67:89:ab", "ip6": None, "ip4": "192.168.0.1", "template": None},
+    "host2->eth0": {"note": "another note", "mac": "01:23:45:67:89:bb", "ip6": None, "ip4": None, "template": None},
+    "host1->eth1": {"note": None, "mac": "01:23:45:67:89:ba", "ip6": None, "ip4": None, "template": None}
 }
 
 def imperative(r):
-	doStuff(r)
+    doStuff(r)
+    doStuff_embed(r)
 
 def doStuff(r):
-	vendorNames = set(["vendor1", "vendor2"])
-	presentVendors = set(r.c(kindInstances("vendor")))
-	vendorNames = vendorNames - presentVendors
+    vendorNames = set(["vendor1", "vendor2"])
+    presentVendors = set(r.c(kindInstances("vendor")))
+    vendorNames = vendorNames - presentVendors
 
-	r.c(startChangeset())
-	for obj in vendorNames:
-		r.cvoid(createObject("vendor", obj))
+    r.c(startChangeset())
+    for obj in vendorNames:
+        r.cvoid(createObject("vendor", obj))
 
-	hardwareNames = set(["hw1", "hw2"])
-	presentHW = set(r.c(kindInstances("hardware")))
-	hardwareNames = hardwareNames - presentHW
+    hardwareNames = set(["hw1", "hw2"])
+    presentHW = set(r.c(kindInstances("hardware")))
+    hardwareNames = hardwareNames - presentHW
 
-	for obj in hardwareNames:
-		r.cvoid(createObject("hardware", obj))
+    for obj in hardwareNames:
+        r.cvoid(createObject("hardware", obj))
 
-	r.cvoid(setAttribute("hardware", "hw1", "vendor", "vendor1"))
-	r.cvoid(setAttribute("hardware", "hw1", "purchase", "2008-10-10"))
-	r.cvoid(setAttribute("hardware", "hw1", "warranty", "2010-10-10"))
-	r.cvoid(setAttribute("hardware", "hw2", "vendor", "vendor1"))
-	r.cvoid(setAttribute("hardware", "hw2", "purchase", "2006-06-06"))
-	r.cvoid(setAttribute("hardware", "hw2", "warranty", "2010-06-06"))
+    r.cvoid(setAttribute("hardware", "hw1", "vendor", "vendor1"))
+    r.cvoid(setAttribute("hardware", "hw1", "purchase", "2008-10-10"))
+    r.cvoid(setAttribute("hardware", "hw1", "warranty", "2010-10-10"))
+    r.cvoid(setAttribute("hardware", "hw2", "vendor", "vendor1"))
+    r.cvoid(setAttribute("hardware", "hw2", "purchase", "2006-06-06"))
+    r.cvoid(setAttribute("hardware", "hw2", "warranty", "2010-06-06"))
 
-	hardwareData = r.c(multipleObjectData("hardware"))
-	r.assertEqual(hardwareData, expectedHardwareData)
-	revision = r.c(commitChangeset("test"))
+    hardwareData = r.c(multipleObjectData("hardware"))
+    for key in hardwareData:
+        r.assertEqual(hardwareData[key], expectedHardwareData[key])
+    revision = r.c(commitChangeset("test"))
 
-	hardwareData = r.c(multipleObjectData("hardware", revision))
-	r.assertEqual(hardwareData, expectedHardwareData)
+    hardwareData = r.c(multipleObjectData("hardware", revision))
+    for key in hardwareData:
+        r.assertEqual(hardwareData[key], expectedHardwareData[key])
 
-	hardwareNames = set(["hw3","hw4"])
-	presentHW = set(r.c(kindInstances("hardware")))
-	hardwareNames = hardwareNames - presentHW
+    hardwareNames = set(["hw3","hw4"])
+    presentHW = set(r.c(kindInstances("hardware")))
+    hardwareNames = hardwareNames - presentHW
 
-	r.c(startChangeset())
+    r.c(startChangeset())
 
-	for obj in hardwareNames:
-		r.cvoid(createObject("hardware", obj))
+    for obj in hardwareNames:
+        r.cvoid(createObject("hardware", obj))
 
-	r.cvoid(setAttribute("hardware", "hw3", "vendor", "vendor2"))
-	r.cvoid(setAttribute("hardware", "hw3", "purchase", "2010-10-10"))
-	r.cvoid(setAttribute("hardware", "hw3", "warranty", "2012-10-10"))
-	r.cvoid(setAttribute("hardware", "hw4", "vendor", "vendor2"))
-	r.cvoid(setAttribute("hardware", "hw4", "purchase", "2010-10-10"))
-	r.cvoid(setAttribute("hardware", "hw4", "warranty", "2012-10-10"))
-	r.c(commitChangeset("test2"))
+    r.cvoid(setAttribute("hardware", "hw3", "vendor", "vendor2"))
+    r.cvoid(setAttribute("hardware", "hw3", "purchase", "2010-10-10"))
+    r.cvoid(setAttribute("hardware", "hw3", "warranty", "2012-10-10"))
+    r.cvoid(setAttribute("hardware", "hw4", "vendor", "vendor2"))
+    r.cvoid(setAttribute("hardware", "hw4", "purchase", "2010-10-10"))
+    r.cvoid(setAttribute("hardware", "hw4", "warranty", "2012-10-10"))
+    r.c(commitChangeset("test2"))
+
+def doStuff_embed(r):
+    hostNames = set(["host1", "host2"])
+    presentHosts = set(r.c(kindInstances("host")))
+    hostNames = hostNames - presentHosts
+
+    r.c(startChangeset())
+    for obj in hostNames:
+        r.cvoid(createObject("host", obj))    
+    
+    interfaceNames = set(["host1->eth0", "host2->eth0"])
+    presentInterfaces = set(r.c(kindInstances("interface")))
+    interfaceNames = interfaceNames - presentInterfaces
+
+    for obj in interfaceNames:
+        r.cvoid(createObject("interface", obj))
+        
+    r.cvoid(setAttribute("interface", "host1->eth0", "mac", "01:23:45:67:89:aa"))
+    r.cvoid(setAttribute("interface", "host1->eth0", "ip4", "192.168.0.1"))
+    r.cvoid(setAttribute("interface", "host1->eth0", "note", "some note"))
+    r.cvoid(setAttribute("interface", "host2->eth0", "mac", "01:23:45:67:89:bb"))
+    r.cvoid(setAttribute("interface", "host2->eth0", "note", "another note"))
+
+    interfaceData = r.c(multipleObjectData("interface"))
+    for key in interfaceData:
+        r.assertEqual(interfaceData[key], expectedInterfaceData[key])
+    revision = r.c(commitChangeset("test"))
+    
+    interfaceData = r.c(multipleObjectData("interface", revision))
+    for key in interfaceData:
+        r.assertEqual(interfaceData[key], expectedInterfaceData[key])
+    
+    interfaceNames = set(["host1->eth1"])
+    presentInterfaces = set(r.c(kindInstances("interface")))
+    interfaceNames = interfaceNames - presentInterfaces
+
+    r.c(startChangeset())
+    for obj in interfaceNames:
+        r.cvoid(createObject("interface", obj))
+        
+    r.cvoid(setAttribute("interface", "host1->eth0", "mac", "01:23:45:67:89:ab"))
+    r.cvoid(setAttribute("interface", "host1->eth1", "mac", "01:23:45:67:89:ba"))
+    interfaceData = r.c(multipleObjectData("interface"))
+    for key in interfaceData:
+        r.assertEqual(interfaceData[key], expectedInterfaceData2[key])
+    r.c(commitChangeset("test"))
+    
+    interfaceData = r.c(multipleObjectData("interface", revision))
+    for key in interfaceData:
+        r.assertEqual(interfaceData[key], expectedInterfaceData[key])
+    
