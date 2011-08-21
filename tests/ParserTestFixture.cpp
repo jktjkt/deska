b8e042c5b2f9e3c09bc2617a665f8b9b82ada7b1
Tomas Hubik <hubik.tomas@gmail.com>
2011-08-21 17:49:59 +0200

    Using correct types for mac and ip

--- tests/ParserTestFixture.cpp
+++ tests/ParserTestFixture.cpp
@@ -38,8 +38,8 @@ ParserTestFixture::ParserTestFixture()
     fake->attrs["hardware"].push_back( KindAttributeDataType( "id", TYPE_INT ) );
     fake->attrs["hardware"].push_back( KindAttributeDataType( "name", TYPE_STRING ) );
     fake->attrs["hardware"].push_back( KindAttributeDataType( "price", TYPE_DOUBLE ) );
-    fake->attrs["interface"].push_back( KindAttributeDataType( "ip", TYPE_STRING ) );
-    fake->attrs["interface"].push_back( KindAttributeDataType( "mac", TYPE_STRING ) );
+    fake->attrs["interface"].push_back( KindAttributeDataType( "ip", TYPE_IPV4_ADDRESS ) );
+    fake->attrs["interface"].push_back( KindAttributeDataType( "mac", TYPE_MAC_ADDRESS ) );
     fake->attrs["host"].push_back( KindAttributeDataType( "hardware_id", TYPE_IDENTIFIER ) );
     fake->attrs["host"].push_back( KindAttributeDataType( "name", TYPE_STRING ) );
     fake->attrs["host"].push_back( KindAttributeDataType( "role", TYPE_IDENTIFIER_SET ) );
