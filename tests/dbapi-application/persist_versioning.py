c675689b2f5a6cd96d77c903e470d90d0bacd7e7
Jan Kundrát <kundratj@fzu.cz>
2011-08-25 14:06:53 +0200

    Deprecating declarative unit tests, step four

--- tests/dbapi-application/persist_versioning.py
+++ tests/dbapi-application/persist_versioning.py
@@ -1,51 +1,46 @@
 from apiUtils import *
 
-vlist = [
-        {"timestamp": CurrentTimestamp(), "commitMessage": "commit-resumed",
-         "revision": Variable("v1"), "author": DeskaDbUser()},
-        {"timestamp": CurrentTimestamp(), "commitMessage": "commit-fresh",
-         "revision": Variable("v2"), "author": DeskaDbUser()}
-    ]
-
-clist = [
-    {"status": "DETACHED", "changeset": Variable("changeset"),
-     "author": DeskaDbUser(), "timestamp": CurrentTimestamp(), "parentRevision":
-     Any(), "message": "detaching"}
-]
-
-declarative = [
+def imperative(r):
     # at first, we can create a changeset
-    startChangeset().returns(Any()),
+    r.c(startChangeset())
     # but trying that once again will fail
-    startChangeset().throws(ChangesetAlreadyOpenError()),
+    r.cfail(startChangeset(), ChangesetAlreadyOpenError())
     # so let's abort the real changeset
-    abortCurrentChangeset(),
+    r.cvoid(abortCurrentChangeset())
     # we won't be able to abort it twice, though
-    abortCurrentChangeset().throws(NoChangesetError()),
+    r.cfail(abortCurrentChangeset(), NoChangesetError())
     # so now we're outside of a changeset
 
     # try to commit, outside of a changeset
-    commitChangeset("commit-outside-a-changeset").throws(NoChangesetError()),
+    r.cfail(commitChangeset("commit-outside-a-changeset"), NoChangesetError())
 
     # try to commit a resumed changeset
-    startChangeset().register("changeset"),
-    detachFromCurrentChangeset("detaching"),
-    detachFromCurrentChangeset("detaching once again").throws(NoChangesetError()),
-    # FIXME: resumeChangeset("xyz").throws(ChangesetParsingError()),
-    resumeChangeset("xyz").throws(ServerError()),
-    pendingChangesets().returns(ListEnd(clist)),
+    changeset = r.c(startChangeset())
+    r.cvoid(detachFromCurrentChangeset("detaching"))
+    r.cfail(detachFromCurrentChangeset("detaching once again"), NoChangesetError())
+    # FIXME: resumeChangeset("xyz"), ChangesetParsingError()),
+    r.cfail(resumeChangeset("xyz"), ServerError())
+    r.assertEqual(r.c(pendingChangesets()),
+        ListEnd([
+            {"status": "DETACHED", "changeset": changeset,
+             "author": DeskaDbUser(), "timestamp": CurrentTimestamp(),
+             "parentRevision": Any(), "message": "detaching"}]))
     # FIXME: call pendingChangesets with a filter
-    resumeChangeset(Variable("changeset")),
-    resumeChangeset(Variable("changeset")).throws(ChangesetAlreadyOpenError()),
-    commitChangeset("commit-resumed").register("v1"),
+    r.cvoid(resumeChangeset(changeset))
+    r.cfail(resumeChangeset(changeset), ChangesetAlreadyOpenError())
+    v1 = r.c(commitChangeset("commit-resumed"))
 
     # try to commit a fresh one
-    startChangeset().register("v2"),
-    commitChangeset("commit-fresh").register("v2"),
-    listRevisions().returns(ListEnd(vlist)),
+    v2 = r.c(startChangeset())
+    v2 = r.c(commitChangeset("commit-fresh"))
+    vlist = [
+        {"timestamp": CurrentTimestamp(), "commitMessage": "commit-resumed",
+         "revision": v1, "author": DeskaDbUser()},
+        {"timestamp": CurrentTimestamp(), "commitMessage": "commit-fresh",
+         "revision": v2, "author": DeskaDbUser()}
+    ]
+    r.assertEqual(r.c(listRevisions()), ListEnd(vlist))
 
     # check filters as well
-    listRevisions({"condition": "columnGe", "metadata": "revision",
-                   "value": Variable("v1")}).returns(vlist),
-
-]
+    r.assertEqual(r.c(listRevisions({"condition": "columnGe", "metadata": "revision",
+                   "value": v1})), vlist)
