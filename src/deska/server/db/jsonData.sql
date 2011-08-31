284637caf8fcd9b5ba64f292e463decd32bcf622
Lukáš Kerpl <luke7@seznam.cz>
2011-08-31 20:02:02 +0200

    Add rest of data functions

--- src/deska/server/db/jsonData.sql
+++ src/deska/server/db/jsonData.sql
@@ -133,76 +133,7 @@ def main(tag,kindName,objectName,revision):
 
 	data = [dutil.mystr(x) for x in data[0]]
 	res = dict(zip(colnames,data))
-	jsn[name] = res
-	return json.dumps(jsn)
-$$
-LANGUAGE python SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION jsn.multipleObjectData(tag text, kindName text, revision text, filter text = NULL)
-RETURNS text
-AS
-$$
-import dutil
-import json
-
-@pytypes
-def main(tag,kindName,revision,filter):
-	name = "multipleObjectData"
-	jsn = dutil.jsn(name,tag)
-
-	# check kind name
-	if kindName not in dutil.generated.kinds():
-		return dutil.errorJson(name,tag,"InvalidKindError","{0} is not valid kind.".format(kindName))
-	
-	atts = dutil.generated.atts(kindName)
-	atts["name"] = "identifier"
-	# dot the atts with kindName
-	new_atts = dict()
-	for att in atts:
-		new_atts[att] = "{0}.{1}".format(kindName,att)
-	atts = new_atts
-
-	embed = dutil.generated.embed()
-	refs = dutil.generated.refs()
-	if kindName in embed:
-		#FIXME: propagate delimiter constant here,or drop this argument
-		coldef = "join_with_delim({ref}_get_name({kind}.{ref}, $1), {kind}.name, '->') AS name".format(ref = embed[kindName], kind = kindName)
-		atts["name"] = coldef
-		# delete embed attribute
-		del atts[embed[kindName]]
-	if kindName in refs:
-		for kind in refs[kindName]:
-			coldef = "{0}_get_name({1},$1)".format(kind,atts[kind])
-			atts[kind] = coldef
-	columns = ",".join(atts.values())
-
-	try:
-		# set start to 2, $1 - version is set
-		filter = dutil.Filter(filter,2)
-		where, values = filter.getWhere()
-		select = 'SELECT '+ columns +' FROM {0}_data_version($1) AS {0} ' + filter.getJoin(kindName) + where
-		select = select.format(kindName)
-	except dutil.DutilException as err:
-		return err.json(name,jsn)
-
-	try:
-		revisionNumber = dutil.fcall("revision2num(text)",revision)
-		args = [revisionNumber]+values
-		colnames, cur = dutil.getdata(select,*args)
-	except dutil.DeskaException as err:
-		return err.json(name,jsn)
-	
-	res = dict()
-	for line in cur:
-		data = [dutil.mystr(x) for x in line]
-		data = dict(zip(atts.keys(),data))
-
-		#FIXME? this shoud be slower, but its in protocol spec.
-		objectName = data['name']
-		del data['name']
-		res[objectName] = data
-
-
+	res = dutil.collectOriginColumns(res)
 	jsn[name] = res
 	return json.dumps(jsn)
 $$
