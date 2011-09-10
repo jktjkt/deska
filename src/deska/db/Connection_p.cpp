7f115f0b7e7c2573cc81ce47b54336d713e732fb
Jan Kundrát <kundratj@fzu.cz>
2011-09-11 00:19:09 +0200

    Use environment variables to control the way of JSON pipe access

--- src/deska/db/Connection_p.cpp
+++ src/deska/db/Connection_p.cpp
@@ -19,36 +19,62 @@
 * Boston, MA 02110-1301, USA.
 * */
 
+#include <boost/lexical_cast.hpp>
 #include <boost/spirit/include/phoenix_bind.hpp>
 #include <cstdlib>
 #include "Connection_p.h"
 #include "ProcessIO.h"
+#include "UnixFdIO.h"
 #include "deska/cli/CliConfig.h"
 
 namespace Deska {
 namespace Db {
 
+#define DESKA_VIA_FD_R "DESKA_VIA_FD_R"
+#define DESKA_VIA_FD_W "DESKA_VIA_FD_W"
+
 Connection_p::Connection_p(): io(0)
 {
-    // FIXME: don't hardcode these
-    std::vector<std::string> args;
-    //std::string test = Cli::CliConfig::getInstance()->getVar<std::string>("DBConnection.Server");
+    char *envUseFd = ::getenv(DESKA_VIA_FD_R);
+    if (envUseFd) {
+        std::string fdRead = ::getenv(DESKA_VIA_FD_R);
+        char *tmp = ::getenv(DESKA_VIA_FD_W);
+        if (!tmp) {
+            throw std::runtime_error("Deska::Db::Connection_p: environment: missing " DESKA_VIA_FD_W);
+        }
+        std::string fdWrite(tmp);
 
-    args.push_back(std::string(CMAKE_CURRENT_SOURCE_DIR) + "/src/deska/server/app/deska_server.py");
+        int rfd = boost::lexical_cast<int>(fdRead);
+        int wfd = boost::lexical_cast<int>(fdWrite);
+        if (rfd < 0) {
+            throw std::runtime_error("Deska::Db::Connection_p: environment: illegal " DESKA_VIA_FD_R);
+        }
+        if (wfd < 0) {
+            throw std::runtime_error("Deska::Db::Connection_p: environment: illegal " DESKA_VIA_FD_W);
+        }
 
-    // FIXME: switch to boost::program_options, see redmine #179
-    char *deska_user = ::getenv("DESKA_USER");
-    if (deska_user) {
-        args.push_back("-U");
-        args.push_back(deska_user);
+        io = new UnixFdIO(rfd, wfd);
+    } else {
+        // FIXME: don't hardcode these
+        std::vector<std::string> args;
+        //std::string test = Cli::CliConfig::getInstance()->getVar<std::string>("DBConnection.Server");
+
+        args.push_back(std::string(CMAKE_CURRENT_SOURCE_DIR) + "/src/deska/server/app/deska_server.py");
+
+        // FIXME: switch to boost::program_options, see redmine #179
+        char *deska_user = ::getenv("DESKA_USER");
+        if (deska_user) {
+            args.push_back("-U");
+            args.push_back(deska_user);
+        }
+        char *deska_db = ::getenv("DESKA_DB");
+        if (deska_db) {
+            args.push_back("-d");
+            args.push_back(deska_db);
+        }
+
+        io = new ProcessIO(args);
     }
-    char *deska_db = ::getenv("DESKA_DB");
-    if (deska_db) {
-        args.push_back("-d");
-        args.push_back(deska_db);
-    }
-
-    io = new ProcessIO(args);
     willRead.connect(boost::phoenix::bind(&IOSocket::readStream, *io));
     willWrite.connect(boost::phoenix::bind(&IOSocket::writeStream, *io));
     wantJustReadData.connect(boost::phoenix::bind(&IOSocket::recentlyReadData, *io));
