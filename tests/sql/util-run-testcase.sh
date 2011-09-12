a040263c93cc2aade024eb458914d1654e5a53ea
Jan Kundrát <kundratj@fzu.cz>
2011-09-12 17:27:59 +0200

    Move the git initialization to a common file

--- tests/sql/util-run-testcase.sh
+++ tests/sql/util-run-testcase.sh
@@ -61,6 +61,18 @@ export DESKA_CFGGEN_GIT_SCRIPTS
 DESKA_CFGGEN_GIT_SECOND=${DESKA_GENERATED_FILES}/second-wd
 export DESKA_CFGGEN_GIT_SECOND
 
+# Initialize the master repository which simulates a remote repo
+git init --bare ${DESKA_CFGGEN_GIT_REPO} || die "git init --bare my_repo failed"
+# Initialize the "primary clone"; that's the repository from which we create extra WCs
+git clone ${DESKA_CFGGEN_GIT_REPO} ${DESKA_CFGGEN_GIT_PRIMARY_CLONE} || die "git clone my_repo my_primary_clone failed"
+# Got to start the master branch now, or git-new-workdir will break
+pushd ${DESKA_CFGGEN_GIT_PRIMARY_CLONE}
+echo "This is a repo of the resulting configuration" > README
+git add README || die "git add README failed"
+git commit -m "Initial commit" || die "git commit failed"
+git push origin master || die "git push origin master failed"
+popd
+
 export DESKA_SOURCES
 
 case "${TESTMODE}" in
