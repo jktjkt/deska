633ef46fb09c1bf88bb2002e58cc9117389ed4e9
Jan Kundrát <kundratj@fzu.cz>
2011-09-14 17:18:40 +0200

    debug: Support displaying the DB log when run with DESKA_TRACE_SQL=1

--- run-standalone-tests.sh
+++ run-standalone-tests.sh
@@ -4,6 +4,14 @@ SHMDIR=/dev/shm/deska-${USER}-$$
 mkdir ${SHMDIR}
 initdb -U postgres -A trust ${SHMDIR}
 echo "listen_addresses = ''" >> ${SHMDIR}/postgresql.conf
+
+if [[ -n "${DESKA_TRACE_SQL}" ]]; then
+    echo "log_destination = stderr
+    log_directory = 'pg_log'
+    logging_collector = on
+    #log_statement = all
+    log_filename = error_log" >> ${SHMDIR}/postgresql.conf
+fi
 postgres -D ${SHMDIR} -F -k ${SHMDIR} > /dev/null 2> /dev/null &
 PGPID=$!
 PGHOST=${SHMDIR}
@@ -19,4 +27,9 @@ done
 ctest --output-on-failure $@
 
 kill $PGPID
+
+if [[ -n "${DESKA_TRACE_SQL}" ]]; then
+    cat "${SHMDIR}/pg_log/error_log"
+fi
+
 rm -rf ${SHMDIR}
