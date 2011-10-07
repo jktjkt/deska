7c3a45a7d3deb906d611c899a6822bde36cc1d6a
Jan Kundrát <kundratj@fzu.cz>
2011-10-08 00:53:06 +0200

    Test filters for pendingChangesets()

--- tests/dbapi-application/simple_filtering.py
+++ tests/dbapi-application/simple_filtering.py
@@ -41,9 +41,10 @@ def imperative(r):
         r.c(commitChangeset("blah %d" % x))
 
     # try all operators
-    for (op, key, res) in [("Ge", 4, [4, 5]), ("Gt", 2, [3, 4, 5]),
-                           ("Lt", 4, [1, 2, 3]), ("Le", 4, [1, 2, 3, 4]),
-                           ("Ne", 3, [1, 2, 4, 5]), ("Eq", 2, [2])]:
+    numericData = [("Ge", 4, [4, 5]), ("Gt", 2, [3, 4, 5]),
+                   ("Lt", 4, [1, 2, 3]), ("Le", 4, [1, 2, 3, 4]),
+                   ("Ne", 3, [1, 2, 4, 5]), ("Eq", 2, [2])]
+    for (op, key, res) in numericData:
         myFilter = {"condition": "column%s" % op, "metadata": "revision", "value": "r%d" % key}
         r.assertEqual([x["revision"] for x in r.c(listRevisions(filter=myFilter))],
                      ["r%d" % x for x in res])
@@ -52,53 +53,17 @@ def imperative(r):
     r.cfail(listRevisions(filter={"xxx": "columnGt"}))
 
 
-def test_011b_filterTest(self):
-		'''test filter for pendingChangeset'''
-		# tmp1 always exists
-		filter = {"condition": "columnEq", "column": "changeset", "value": "tmp1", "kind": "metadata"}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.OK)
-		# number of changesets - 1
-		filter = {"condition": "columnNe", "column": "changeset", "value": "tmp1", "kind": "metadata"}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.OK)
-		changesets1 = res.result()
-		# number of changesets - 1 / other method
-		filter = {"condition": "columnGt", "column": "changeset", "value": "tmp1", "kind": "metadata"}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.OK)
-		changesets2 = res.result()
-		# compare 2 results
-		self.assertEqual(changesets1,changesets2)
-
-		# 2 changesets
-		filter = {"operator": "or", "operands": [
-			{"condition": "columnEq", "column": "changeset", "value": "tmp1", "kind": "metadata"},
-			{"condition": "columnEq", "column": "changeset", "value": "tmp2", "kind": "metadata"},
-			{"condition": "columnEq", "column": "changeset", "value": "tmp3", "kind": "metadata"}
-			]}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.OK)
-		changesets1 = res.result()
-		filter = {"operator": "and", "operands": [
-			{"condition": "columnGe", "column": "changeset", "value": "tmp1", "kind": "metadata"},
-			{"condition": "columnLe", "column": "changeset", "value": "tmp3", "kind": "metadata"}
-			]}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.OK)
-		changesets2 = res.result()
-		self.assertEqual(changesets1,changesets2)
-
-		# bad syntax of filter
-		filter = {"xxx": "columnGt", "column": "changeset", "value": "tmp1", "kind": "metadata"}
-		res = self.command(js.pendingChangesets,filter)
-		self.OK(res.otherError)
-
-		# test all conditions
-		for cond in ["columnGe","columnLe","columnGt","columnLt","columnNe","columnEq"]:
-			filter = {"condition": cond, "column": "changeset", "value": "tmp1", "kind": "metadata"}
-			res = self.command(js.pendingChangesets,filter)
-			self.OK(res.OK)
+    # now test a few pending changesets
+    # situation is a bit more complicated, because the first changeset we create
+    # is not really tmp1, so we will not filter against the key, but against the
+    # "message"
+    for x in range(1, 6):
+        r.c(startChangeset())
+        r.cvoid(detachFromCurrentChangeset("%d" % x))
+    for (op, key, res) in numericData:
+        myFilter = {"condition": "column%s" % op, "metadata": "message", "value": "%d" % key}
+        r.assertEqual([x["message"] for x in r.c(pendingChangesets(filter=myFilter))],
+                     ["%d" % x for x in res])
 
 def test_012_filterJoinTest(self):
 		'''test joining for listRevisions and pendingChangeset'''
