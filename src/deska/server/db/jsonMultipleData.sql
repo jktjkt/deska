0c28d812f6f94ac3846ce899f25027ef0e7594c8
Lukáš Kerpl <luke7@seznam.cz>
2011-11-10 22:56:29 +0100

    Update getting name from ref_uid after last commit

--- src/deska/server/db/jsonMultipleData.sql
+++ src/deska/server/db/jsonMultipleData.sql
@@ -204,14 +204,10 @@ def main(tag,kindName,revision,filter):
 		if refs[relName] == kindName:
 			refTbl = dutil.generated.relToTbl(relName)
 			refCol = dutil.generated.relFromCol(relName)
-			#if dutil.generated.atts(kindName)[refTbl] == "identifier_set":
-			#	#"inner_host_service_multiRef_get_set"
-			#	coldef = "inner_{0}_{1}_multiRef_get_set({0}.uid, $1)".format(kindName, refTbl)
-			#else:
-			#	coldef = "{0}_get_name({1},$1)".format(refTbl,refCol)
-			#	coldef = atts[refCol]
-			#atts[refCol] = coldef
-			# No action here is now required, but filter will fail with this data function
+			if dutil.generated.atts(kindName)[refTbl] != "identifier_set":
+				# No action for identifier_set required, getting from data function
+				coldef = "{0}_get_name({1},$1)".format(refTbl,refCol)
+				atts[refCol] = coldef
 
 	columns = ",".join(atts.values())
 
