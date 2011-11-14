62e48d00a5c6efafbdaa02292e3fb9d52c461bd7
Martina Krejcova <martinka.krejcova@seznam.cz>
2011-11-14 11:56:20 +0100

    composition - add modul virtual_hardware

---
+++ install/modules/demo/021-virtual_hardware.sql
@@ -0,0 +1,22 @@
+--
+-- every module must be place in schema production
+--
+SET search_path TO production,deska;
+
+CREATE SEQUENCE virtual_hardware_uid START 1;
+
+-- virtual hardware
+CREATE TABLE virtual_hardware (
+	-- this column is required in all plugins
+	uid bigint DEFAULT nextval('virtual_hardware_uid')
+		CONSTRAINT virtual_hardware_pk PRIMARY KEY,
+	-- this column is required in all plugins
+	name identifier
+		CONSTRAINT "virtual_hardware with this name already exists" UNIQUE NOT NULL,
+	-- how many CPUs are assigned to this machine?
+	cpu_num int,
+	-- how much RAM (in MB) to put in there?
+	ram int,
+    host bigint
+);
+
