1bb71a3e9042b9c2cb0cf7acc04a005b1ae300de
Jan Kundrát <kundratj@fzu.cz>
2011-11-27 02:58:19 +0100

    Change ticket reference
    refs #346

--- tests/dbapi-application/concurrent-versioning.py
+++ tests/dbapi-application/concurrent-versioning.py
@@ -56,5 +56,5 @@ def imperative(r):
     r.cvoid(unlockCurrentChangeset(), conn1)
     r.cvoid(resumeChangeset(changeset), conn=conn3)
     # Now the first session shall not be able to commit the changeset
-    # FIXME: Redmine #308
+    # FIXME: Redmine #346
     #r.cfail(commitChangeset("."), conn=conn1, exception=ChangesetLockingError())
