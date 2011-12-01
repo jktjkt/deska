8f1754c73face31634f4c0996d71d359c5befa82
Jan Kundrát <kundratj@fzu.cz>
2011-12-01 17:47:15 +0100

    test: concurrency and the config generators
    refs #337

--- tests/dbapi-application/config_generators.py
+++ tests/dbapi-application/config_generators.py
@@ -1,10 +1,15 @@
 '''Test that the server-side configuration generators work reasonably'''
 
 import os
+import sys
 from apiUtils import *
 import datetime
 import deska
 import subprocess
+import tempfile
+import fcntl
+import select
+from testdbapi import Connection, json, deunicodeify
 
 PATH_WC = os.environ["DESKA_CFGGEN_GIT_WC"]
 PATH_SECOND = os.environ["DESKA_CFGGEN_GIT_SECOND"]
@@ -166,8 +171,81 @@ f.write("pwnzor\\n")
     r.assertEqual(file(mytarget, "rb").readlines(), ["pwnzor\n"])
     rmGenerator("01")
 
+def test_parallel_commit(r):
+    """Test what happens when the config generators take a long time to complete
+
+    This is going to be rather complicated. We want to have two parallel
+    sessions running at the same time. The first of them will start a changeset,
+    perform some changes and trigger the cfggen process, but this process will
+    take "quite some time" to finish. While the generators are running, the
+    parallel session attempts to steal the changeset. That shall fail.
+    """
+
+    lockA = tempfile.mkstemp()[1]
+    lockDir = tempfile.mkdtemp()
+    fifo = os.path.join(lockDir, "l")
+    os.mkfifo(fifo)
+
+    writeGenerator("sleeping-beauty",
+"""#!/usr/bin/env python
+import fcntl
+import os
+fA = file("%s", "ab")
+fB = file("%s", "wb")
+fB.write("x")
+fB.flush()
+fcntl.lockf(fA.fileno(), fcntl.LOCK_EX)
+file("output", "wb").write("All done\\n")
+""" % (lockA, fifo))
+
+    fA = file(lockA, "ab")
+    fcntl.lockf(fA.fileno(), fcntl.LOCK_EX)
+
+    conn1 = Connection(r.cmd)
+    conn2 = Connection(r.cmd)
+
+    changeset = r.c(startChangeset(), conn=conn1)
+    r.c(createObject("vendor", "dummy06"), conn=conn1)
+    # We have to go the manual route here. We cannot block now, so we have to
+    # feed the data directly.
+    print >>sys.stderr, "*** Config generator will work in %s" % PATH_WC
+    print >>sys.stderr, "*** Starting the config generator in the background"""
+    conn1.p.stdin.write("{\"tag\":\"HHH\",\"command\":\"showConfigDiff\"}\r\n")
+    conn1.p.stdin.flush()
+    print >>sys.stderr, "*** Cfg generator started."""
+    # At this point, the process associated with the conn1 shall begin executing
+    # the generators.
+    # Let's wait for the generator to come to the life
+    print >>sys.stderr, "*** About to open a FIFO"
+    fp = os.open(fifo, os.O_RDONLY)
+    print >>sys.stderr, "*** FIFO opened, reading from there"
+    r.assertEqual(os.read(fp, 1), "x")
+    print >>sys.stderr, "*** Read data, which means that the cfg generator is alive and kicking"
+
+    # At this point, the generator is blocking and we can do our best to beat
+    # the hell out of the DB from a second connection.
+    r.cfail(resumeChangeset(changeset), conn=conn2, exception=ChangesetLockingError())
+
+    # Let's unblcok the generator and let it continue
+    print >>sys.stderr, "*** Unblocking the cfg generator in conn1"
+    fcntl.lockf(fA.fileno(), fcntl.LOCK_UN)
+
+    # ...and process the command we sent in there
+    print >>sys.stderr, "*** Waiting for the cfg generator to respond"""
+    status = select.select((conn1.p.stdout, conn1.p.stderr), (), ())
+    if (conn1.p.stderr in status[0]):
+        err = os.read(conn1.p.stderr.fileno(), 65536)
+        print >>sys.stderr, err
+        r.assertTrue(False)
+    cmdres = deunicodeify(json.loads(conn1.p.stdout.readline()))
+    r.assertEqual(sorted(cmdres.keys()), sorted(['tag', 'response', 'showConfigDiff']))
+    r.assertTrue(cmdres["showConfigDiff"].index("All done") != -1)
+    rmGenerator("sleeping-beauty")
+
+
 def imperative(r):
     test_trivial(r)
     test_no_generators(r)
     test_no_output(r)
     test_unchanged_output(r)
+    test_parallel_commit(r)
