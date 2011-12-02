jktjkt/deska

commit 27f08c71ecc12439d28202ec624b004c08aebd8d

template - doxygen comments

install/modules/demo/02-hardware.sql
@@ -30,6 +30,6 @@ CREATE TABLE hardware (
 	-- add cpu type, when we have cpu type table
 	host bigint,
 	note_hardware text,
-	template_hardware bigint
+	template_hardware text
 );
 

src/deska/server/db/gen_sql/template.py
@@ -1,14 +1,28 @@
 
 class Template:
-	table_query_str = "SELECT relname FROM get_table_info() WHERE attname LIKE 'template_%';"
+	"""This class contains all functions needed for the generation of the template table.
+	These template tables are generated for tables that have a attribute with prefix "template_" and could be templated.
+	"""
+	table_query_str = "SELECT relname, typname FROM get_table_info() WHERE attname LIKE 'template_%';"
+	"""Query to get names of tables that would be templated.
+	For those tables we will generate tbl_template from which they will inherit values.
+	"""
 	not_null_query_str = "SELECT n_constraints_on_table('%(tbl)s');"
+	"""Query to get names of attributes of table tbl, which have not null constraint.
+	We will drop these constraints. In the template tables we allow null values.
+	"""
 	relations_str = "SELECT relation, attname, refkind FROM kindrelations_full_info('%s');"
+	"""Query to get information about relations of the table.
+	We will generate template tables for identifiers sets' inner tables. Drop columns which could not be templated - columns which are in contains, containable or embed into relation.
+	"""
 	template_columns_str = "SELECT relname, attname FROM table_info_view WHERE attname LIKE 'template_%';"
+	"""Query to get name of attribute with "tempalte_" prefix for each table that has it.
+	We will drop these constraints. In template table we allow null values.
+	"""
 	table_columns_str = "SELECT attname FROM kindattributes('%(tbl)s') WHERE attname NOT LIKE 'template_%%';"
-	
-	"""Query to get names of tables which are merged with this table."""
-
-
+	"""Query to get all attributes of the given templated table.
+	They are used to find out if there is at least one attribute that could be templated and could inherit value from template.
+	"""
 	template_str = '''
 CREATE SEQUENCE production.%(tbl)s_template_uid;
 CREATE TABLE production.%(tbl)s_template (
@@ -21,14 +35,23 @@ ALTER TABLE %(tbl)s_template ALTER COLUMN uid SET DEFAULT nextval('%(tbl)s_templ
 ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") REFERENCES %(tbl)s_template(uid) DEFERRABLE INITIALLY IMMEDIATE;
 
 '''
-
+	"""Create table string and do appropriate modifications.
+	We will create template table for tables that have to be templated. Template tables would be like the table that would be templated by them.
+	Constraint with rtempl_ prefix will be added to template table. This constraint will refer to uid of the same table. It ensures that template would be templated to.
+	"""
 	drop_column_str = "ALTER TABLE %s_template DROP COLUMN %s;"
+	"""Drop column string.
+	"""
 
 	drop_not_null_str = "ALTER TABLE %s_template ALTER COLUMN %s DROP NOT NULL;"
+	"""Drop not null constraint.
+	"""
 
 	add_multiref_fk_str = '''ALTER TABLE %(tbl_name)s_template 
 	ADD CONSTRAINT rset_%(tbl_name)s_fk_%(column)s FOREIGN KEY (%(column)s)
 	REFERENCES %(reftbl_name)s (uid) DEFERRABLE INITIALLY IMMEDIATE;'''
+	"""Add rset_ constraint to columns that refers to identifier set.
+	"""
 
 	def __init__(self,db_connection):
 		self.plpy = db_connection;
@@ -39,8 +62,8 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 
 		# select all tables
 		record = self.plpy.execute(self.table_query_str)
-		for tbl in record:
-			self.tables.add(tbl[0])
+		for row in record:
+			self.tables.add(row[0])
 
 		self.not_null_dict = dict()
 		for table_name in self.tables:
@@ -62,6 +85,9 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 
 	# generate sql for all tables
 	def gen_templates(self,filename):
+		"""Generates create table statement for template table and modification needed for having well defined template table.
+		Template table will be created like table that will be templated by it. So created template table will have than dropped not null constraints and dropped columns that won't be in template table.
+		"""
 		self.sql = open(filename,'w')
 
 		# print this to add proc into genproc schema
@@ -75,7 +101,7 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 		return
 
 	def gen_relation_modif(self, table_name):
-		"""Generates sql code for drop column statemnets and adds refers to set relation to template tables.
+		"""Generates sql code for drop column statements and adds refers to set relation to template tables.
 		
 		Drop column that is apart of the embed into relation and columns that are a part of some contains or containable relation.
 		Adds foreign key constraint to columns that should refers to set. This ensures that the inner table for n:m relation - multirefence will be created for this template table.
@@ -110,12 +136,14 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 
 
 	def gen_drop_embed_parent_column(self, table_name, embed_column):
+		"""The column that refers to another table with embed into relation is not templated"""
 		if embed_column <> "":
 			return self.drop_column_str % (table_name, embed_column)
 		else:
 			return ""
 
 	def gen_drop_contained_column(self, table_name, contained_columns):
+		"""Not null constraints inherited from the table that thisone templates are dropped, compulsory columns need not to be defined in template table."""
 		drop_contained_columns = ""
 		for column in contained_columns:
 			drop_contained_columns = drop_contained_columns + '\n' + self.drop_column_str % (table_name, column)
@@ -123,6 +151,7 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 		return drop_contained_columns
 
 	def gen_refers_to_set(self, table_name, refers_to_set):
+		"""Create templates for identifier set inner tables. Values that are inserted into attrributes of identifier_set type could be defined by template too."""
 		add_multiref_fk = ""
 		for column in refers_to_set:
 			reftbl = refers_to_set[column]
@@ -132,10 +161,12 @@ ALTER TABLE %(tbl)s ADD CONSTRAINT rtempl_%(tbl)s FOREIGN KEY ("%(templ_col)s") 
 
 	# generate sql for one table
 	def gen_table_template(self,table_name):
+		"""Create template table."""
 		template_column = self.template_columns[table_name]
 		return self.template_str % {'tbl': table_name, 'templ_col': template_column}
 
 	def gen_table_drop_not_null(self, table_name):
+		"""Generates drop not null columns statements"""
 		constraints = list(
 			[self.drop_not_null_str % (tbl, col) for (tbl, col) in
 				zip(([table_name] * len(self.not_null_dict[table_name])),

commit c94b2a08afc7bb944b3d7d0fb60218d01e045fd6

Fix a regression introduced in 27f08c71ecc12439d28202ec624b004c08aebd8d

install/modules/demo/02-hardware.sql
@@ -30,6 +30,6 @@ CREATE TABLE hardware (
 	-- add cpu type, when we have cpu type table
 	host bigint,
 	note_hardware text,
-	template_hardware text
+	template_hardware bigint
 );
 