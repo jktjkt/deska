a02fde21f388ac0b0de266df20ba334f7422be4c
Jan Kundrát <kundratj@fzu.cz>
2011-12-02 18:10:06 +0100

    Add a single-purpose script which nukes the DB
    Yes, this is a duplicate script to the deska_installer.sh.

---
+++ install/deska-unsupported-nuke-db.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while [[ -n "${1}" ]]; do
+	case "${1}" in
+		-d|--database)
+			DATABASE="${2}"
+			shift
+			;;
+		-U|--user)
+			USER="${2}"
+			shift
+			;;
+	esac
+	shift
+done
+
+if [[ -z "${USER}" ]]; then
+	echo "User was not specified" >&2
+	help
+	exit 1
+fi
+
+if [[ -z "${DATABASE}" ]]; then
+	echo "Database was not specified" >&2
+	help
+	exit 1
+fi
+
+for scheme in deska test jsn api production history genproc versioning; do
+    psql -d "${DATABASE}" -U "${USER}" -c "DROP SCHEMA ${scheme} CASCADE;"
+done
