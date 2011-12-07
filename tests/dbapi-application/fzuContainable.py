b0e7bb6f26ffcdf7875e574a9794f9736cfb5924
Jan Kundrát <kundratj@fzu.cz>
2011-12-07 15:11:37 +0100

    test: furter tests for the containable relation

--- tests/dbapi-application/fzuContainable.py
+++ tests/dbapi-application/fzuContainable.py
@@ -5,7 +5,12 @@ import sys
 
 def imperative(r):
     r.assertEqual(r.c(startChangeset()), "tmp1")
+    r.c(createObject("modelhardware", "fake"))
     r.assertEqual(r.c(createObject("hardware", "foo")), "foo")
+    r.cvoid(setAttribute("hardware", "foo", "modelhardware", "fake"))
     r.assertEqual(r.c(createObject("switch", "foo")), "foo")
+    # The DB will not know where to associate this object
     r.cfail(createObject("box", "foo"), exception=ConstraintError())
-    #r.assertEqual(r.c(commitChangeset(".")), "r1")
+    r.assertEqual(r.c(createObject("host", "foo")), "foo")
+    exc = r.cfail(commitChangeset("."), exception=ConstraintError())
+    r.assertTrue(exc["message"].find("null value in column \"box\" violates not-null constraint") != -1)
