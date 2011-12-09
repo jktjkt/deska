7a35df3b1204c4ef5f247cbe0a344871eb976630
Jan Kundrát <kundratj@fzu.cz>
2011-12-09 17:01:00 +0100

    Actually enable the improvements :)
    fixes #382

--- src/deska/cli/CliCommands.cpp
+++ src/deska/cli/CliCommands.cpp
@@ -940,7 +940,7 @@ bool Restore::operator()(const std::string &params)
     ui->currentChangeset = ui->m_dbInteraction->createNewChangeset();
     try {
         ui->m_dbInteraction->lockCurrentChangeset();
-        ui->m_dbInteraction->setApiCachingMode(Db::CACHE_SAME_OBJECT);
+        ui->m_dbInteraction->setApiCachingMode(Db::CACHE_SAME_KIND);
         while (!getline(ifs, line).eof()) {
             if (ifs.fail()) {
                 ui->io->reportError("Reading of backup file failed.");
@@ -1006,7 +1006,7 @@ bool Restore::operator()(const std::string &params)
                 // Got to explicitly flush the cache at this point
                 ui->m_dbInteraction->setApiCachingMode(Db::SEND_IMMEDIATELY);
                 Db::RevisionId realRev = ui->m_dbInteraction->restoringCommit(message, author, timestampConv);
-                ui->m_dbInteraction->setApiCachingMode(Db::CACHE_SAME_OBJECT);
+                ui->m_dbInteraction->setApiCachingMode(Db::CACHE_SAME_KIND);
                 if (realRev != *checkRev) {
                     std::ostringstream ostr;
                     ostr << "Commited revision " << realRev << " differs from " << *checkRev << " stated in commit header.";
