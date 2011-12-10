b46603ac17e01ae903013d91eec7ca02581a4c9f
Martina Krejcova <martinka.krejcova@seznam.cz>
2011-12-10 20:35:23 +0100

    enable testcase refs #401

--- tests/dbapi-application/fzuContainable.py
+++ tests/dbapi-application/fzuContainable.py
@@ -8,14 +8,13 @@ def imperative(r):
     r.c(createObject("modelhardware", "fake"))
     r.assertEqual(r.c(createObject("hardware", "foo")), "foo")
     r.cvoid(setAttribute("hardware", "foo", "modelhardware", "fake"))
-    # FIXME: Redmine #401, even a deleted object blocks creating proper objects
-    ##r.assertEqual(r.c(createObject("switch", "foo")), "foo")
+    r.assertEqual(r.c(createObject("switch", "foo")), "foo")
     # The DB will not know where to associate this object
-    ##r.cfail(createObject("box", "foo"), exception=ConstraintError())
+    r.cfail(createObject("box", "foo"), exception=ConstraintError())
     r.assertEqual(r.c(createObject("host", "foo")), "foo")
     exc = r.cfail(commitChangeset("."), exception=ConstraintError())
     r.assertTrue(exc["message"].find("null value in column \"box\" violates not-null constraint") != -1)
 
-    ##r.cvoid(deleteObject("switch", "foo"))
+    r.cvoid(deleteObject("switch", "foo"))
     r.assertEqual(r.c(createObject("box", "foo")), "foo")
     r.assertEqual(r.c(commitChangeset(".")), "r2")
