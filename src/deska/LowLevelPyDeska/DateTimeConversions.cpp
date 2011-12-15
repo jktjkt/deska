854e90afea8849c9b8384f63a58d9d4ecc8db4df
Jan Kundrát <kundratj@fzu.cz>
2011-12-15 19:29:57 +0100

    Silence a build warning
    ...which was about an attempt to silence a warning being ignored. At least that
    4.1 gcc (el5) does not warn here in the first place :).

--- src/deska/LowLevelPyDeska/DateTimeConversions.cpp
+++ src/deska/LowLevelPyDeska/DateTimeConversions.cpp
@@ -210,7 +210,9 @@ struct tduration_from_python_delta
 };
 
 #ifdef __GNUC__
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
 #pragma GCC diagnostic ignored "-Wwrite-strings"
+#endif
 #endif
 void bind_datetime()
 {
