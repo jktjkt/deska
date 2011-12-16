76ab2ec7da26fe867c9345c740da76cc04037cf9
Lukáš Kerpl <luke7@seznam.cz>
2011-12-16 10:33:14 +0100

    Pass empty array in sql way, huh really strange
    Fixes #413

--- src/deska/server/db/jsonObjects.sql
+++ src/deska/server/db/jsonObjects.sql
@@ -26,7 +26,6 @@ def main(tag,kindName,objectName,attributeName,attributeData):
 			if attributeData == '{}':
 				'''This is hack (see #410), to work with empty string, which psycopg call as array
 				but database thinks it is a string, do not raise exception'''
-				attributeData = []
 				fname = kindName+"_set_"+attributeName+"(text,text[])"
 			else:
 				raise dutil.DutilException("NotASetError","Attribute '{0}' type is identifier_set, but data type is {1}".format(attributeName,attributeData))
