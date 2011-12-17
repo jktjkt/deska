d08e45951db8d625f7db2d5b66d60475ae3e6f18
Jan Kundrát <kundratj@fzu.cz>
2011-12-17 17:51:40 +0100

    dump: updates from the farmdb

--- doc/farmdb/dbo.Networks.Table.sql
+++ doc/farmdb/dbo.Networks.Table.sql
@@ -1,15 +1,15 @@
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'0dbc6c7f-99f8-4ca5-ae69-968c6655d996', N'FZU', 0x93E71A00, 200, 23, NULL)
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'12001c79-93a2-43c4-9833-59ec442f436c', N'Monitoring', 0xC0A80000, 8, 16, NULL)
-INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'27f66a81-e61b-4bb3-ab0f-f290217646a0', N'Golias 25.x', 0x93E71900, 25, 24, NULL)
+INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'27f66a81-e61b-4bb3-ab0f-f290217646a0', N'Golias-25', 0x93E71900, 25, 24, NULL)
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'3a9e668d-e858-47c0-9998-3c9846838134', N'svypocty', 0x0A1ACA00, 202, 24, N'ustavni sit pro vypocty')
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'479b8233-ce93-44fe-aaba-1f33e213e6ce', N's', 0x0A1AC900, 201, 24, NULL)
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'6d31f21c-ceaf-4c22-9269-1730cd686456', N'Golias-NAT', 0xAC100000, 172, 16, N'')
 INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'9751c60e-c319-45c5-a6c1-22fa7e63c4cb', N'ss', 0x0A1ACD00, 205, 24, N'sit FZU pro ovladani prepinacu
 ')
-INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'cd3aa385-2c77-4521-97b4-fc49e6806e04', N'ss', 0x0A1ACD00, 205, 24, N'sit administrace switchu
+INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'cd3aa385-2c77-4521-97b4-fc49e6806e04', N'ss2', 0x0A1ACD00, 205, 24, N'sit administrace switchu
 
 ignore:
 x-deska-quote: "
 x-deska-apos: ''
 ')
-INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'f90729d8-4b01-4a87-8b3c-3fbea8acaddf', N'IPv6 testing', 0x200107181E0117250000000000000000, 625, 64, NULL)
+INSERT [dbo].[Networks] ([NetworkUID], [NetworkName], [NetworkIP], [NetworkVLAN], [NetworkMaskBits], [NetworkNote]) VALUES (N'f90729d8-4b01-4a87-8b3c-3fbea8acaddf', N'IPv6-testing', 0x200107181E0117250000000000000000, 625, 64, NULL)
