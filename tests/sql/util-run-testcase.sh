a0bb6553648090e84cab614514d5fec7b1397849
Jan Kundrát <kundratj@fzu.cz>
2011-12-17 03:08:21 +0100

    tests: use the SQL-level DB restoring to speed up the execution
    This commit currently breaks tests which run on a different scheme than the demo
    one.

--- tests/sql/util-run-testcase.sh
+++ tests/sql/util-run-testcase.sh
@@ -33,28 +33,46 @@ if [[ -z $TESTCASE ]]; then
     die "No test case to run. Execution"
 fi
 
-if [[ -z "${DESKA_GENERATED_FILES}" ]]; then
-    # do not pollute the source tree with generated files
-    DESKA_GENERATED_FILES=`mktemp -d`
-    trap "rm -rf $DESKA_GENERATED_FILES" EXIT
-fi
-
 DESKA_SERVER_SIDE_DESTINATION="${DESKA_GENERATED_FILES}/target"
 
+if [[ -f "${DESKA_TEST_VANILLA_DB}" ]]; then
+    # Restore the DB quickly
+    pg_restore -U "${DESKA_SU}" -d "${DESKA_DB}" --clean --exit-on-error "${DESKA_TEST_VANILLA_DB}" \
+        || die "Restoring the DB"
+else
+    psql -q -U $DESKA_SU -c "CREATE USER ${DESKA_USER};" || die "Create user"
+    psql -q -U $DESKA_SU -c "CREATE USER ${DESKA_ADMIN};" || die "Create admin"
+    for role in deska_admin deska_user; do
+        psql -q -U $DESKA_SU -c "GRANT ${role} TO ${DESKA_ADMIN};" || die "Grant ${role}"
+    done
+    psql -q -U $DESKA_SU -c "GRANT deska_user TO ${DESKA_USER};" || die "Grant ${role}"
 
-# Always remove stuff, so that a possible error in previous test does not leave
-# stuff broken
-./util-delete-database.sh 2>/dev/null
+    if [[ -z ${DESKA_TABLESPACE} ]]; then
+        TABLESPACE=""
+    else
+        TABLESPACE=" WITH TABLESPACE = ${DESKA_TABLESPACE}"
+    fi
+    psql -q -U $DESKA_SU -c "CREATE DATABASE ${DESKA_DB} ${TABLESPACE} OWNER deska_admin;" || die "Create database"
 
-./util-create-database.sh || die "Preparing the DB environment"
+    PGTAP_FILE="${DESKA_SOURCES}/install/pgtap.sql"
+    PGPYTHON_FILE="${DESKA_SOURCES}/install/pgpython.sql"
+    psql -q -U $DESKA_SU -d $DESKA_DB -v ON_ERROR_STOP=1 -f $PGPYTHON_FILE || die "Enable PgPython"
+    psql -q -U $DESKA_ADMIN -d $DESKA_DB -v ON_ERROR_STOP=1 -f $PGTAP_FILE || die "Installing pgTAP"
 
-pushd "${DESKA_SOURCES}/install"
-./deska-deploy-database.sh -U "${DESKA_SU}" -d "${DESKA_DB}" -t "${DESKA_SERVER_SIDE_DESTINATION}" || die "Running deploy-database"
-popd
+    pushd "${DESKA_SOURCES}/install"
+    ./deska-deploy-database.sh -U "${DESKA_SU}" -d "${DESKA_DB}" -t "${DESKA_SERVER_SIDE_DESTINATION}" || die "Running deploy-database"
+    popd
+
+    # Now take a DB backup for the following executions
+    pg_dump -U "${DESKA_SU}" -f "${DESKA_TEST_VANILLA_DB}" \
+        --format=custom --oids "${DESKA_DB}" > /dev/null \
+        || die "Cannot take a DB backup for further tests"
+fi
 
 if [[ -n "${DESKA_WITH_GIT}" ]]; then
     . ./util-manage-git.sh || die "git stuff"
-    deska_init_git "${DESKA_GENERATED_FILES}"
+    rm -rf "${DESKA_GENERATED_FILES}/git"
+    deska_init_git "${DESKA_GENERATED_FILES}/git"
     export GIT_PYTHON_TRACE=full
 fi
 
