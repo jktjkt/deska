770db321eb3a8cbfa499d3326a37527f43a6179c
Jan Kundrát <kundratj@fzu.cz>
2011-12-30 11:55:02 +0100

    cfggen: use insecure permissions to play "well" with multiple users
    The automated testing has not identified an issue with the permissions on the
    git directories when the user who installed them is someone else than the uid
    who runs them. Fail.
    
    This change at least allows the configuration generators to run.

--- tests/sql/util-manage-git.sh
+++ tests/sql/util-manage-git.sh
@@ -27,14 +27,15 @@ deska_init_git()
     export GIT_AUTHOR_EMAIL="unit.test@example.org"
 
     # Initialize the master repository which simulates a remote repo
-    git init --bare ${DESKA_CFGGEN_GIT_REPO} || deska_git_die "git init --bare my_repo failed"
+    git init --bare --shared=all ${DESKA_CFGGEN_GIT_REPO} || deska_git_die "git init --bare my_repo failed"
 
     # Set up a repo-side hook for auto-updating the second clone
-    echo -e "#!/bin/bash\nGIT_WORK_TREE="${DESKA_CFGGEN_GIT_SECOND}" git checkout -f" > \
+    echo -e "#!/bin/bash\numask 000\nGIT_WORK_TREE="${DESKA_CFGGEN_GIT_SECOND}" git checkout -f" > \
         "${DESKA_CFGGEN_GIT_REPO}/hooks/post-receive"
     chmod +x "${DESKA_CFGGEN_GIT_REPO}/hooks/post-receive"
     mkdir "${DESKA_CFGGEN_GIT_SECOND}" || deska_git_die "creating the DESKA_CFGGEN_GIT_SECOND failed"
 
+    umask 000
     # Initialize the "primary clone"; that's the repository from which we create extra WCs
     git clone ${DESKA_CFGGEN_GIT_REPO} ${DESKA_CFGGEN_GIT_PRIMARY_CLONE} ||
         deska_git_die "git clone my_repo my_primary_clone failed"
@@ -45,10 +46,12 @@ deska_init_git()
     git commit -m "Initial commit" || deska_git_die "git commit failed"
     git push origin master || deska_git_die "git push origin master failed"
     popd
+    umask 022
 
     # Prepare (empty) generating scripts
     mkdir "${DESKA_CFGGEN_SCRIPTS}"
 
     # Prepare the root for the working directories (one per changeset)
     mkdir "${DESKA_CFGGEN_GIT_WC}"
+    chmod 0777 "${DESKA_CFGGEN_GIT_WC}"
 }
