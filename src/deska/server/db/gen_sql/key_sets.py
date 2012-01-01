c807fd4e3aee7c6033db75b286dd79bbe8dbf21f
Lukáš Kerpl <luke7@seznam.cz>
2012-01-01 17:00:51 +0100

    Python style comments

--- src/deska/server/db/gen_sql/key_sets.py
+++ src/deska/server/db/gen_sql/key_sets.py
@@ -2,13 +2,13 @@
 
 import string
 
-# class for storing primary key constraints
 class PkSet(dict):
+	'''class for storing primary key constraints'''
 	def __init__(self):
 		dict.__init__(self)
 
-	# own set item - can insert more items into one key
 	def __setitem__(self,name,att):
+		'''own set item - can insert more items into one key'''
 		if self.has_key(name):
 			# if key already exist, append value into item list
 			s = dict.__getitem__(self,name)
@@ -20,8 +20,8 @@ class PkSet(dict):
 			s.append(att)
 			dict.__setitem__(self,name,s)
 
-# class for storing foreign key constraints
 class Fks:
+	'''class for storing foreign key constraints'''
 	def __init__(self):
 		# attributes in key from local table
 		self.att = PkSet()
@@ -30,19 +30,15 @@ class Fks:
 		# dictionary of local table: referenced table
 		self.tbl = dict()
 
-	# insert tuple of table, local attribute, referenced table, referenced attribute
 	def add(self,name,att,table,ratt):
+		'''insert tuple of table, local attribute, referenced table, referenced attribute'''
 		self.att[name] = att
 		self.tbl[name] = table
 		self.ratt[name] = ratt
 
-	# generate alter table for creating foreign key constraints
 	def gen_fkcon(self,con):
-		# add version column into key constraint
-
-		# workround for add fks to objects from earlier changesets
-		#self.att[con] = "version"
-		#self.ratt[con] = "version"
+		'''generate alter table for creating foreign key constraints
+		add version column into key constraint'''
 
 		s = "CONSTRAINT history_%(name)s FOREIGN KEY (%(att)s) REFERENCES %(rtbl)s_history(%(ratt)s) DEFERRABLE INITIALLY DEFERRED"
 		atts = ",".join(self.att[con])
@@ -50,16 +46,16 @@ class Fks:
 		s = s % {'name': con, 'rtbl': self.tbl[con], 'att': atts, 'ratt': ratts}
 		return "ALTER TABLE %(tbl)s_history ADD " + s
 
-	# generate all constraints
 	def gen_fk_constraints(self):
+		'''generate all constraints'''
 		constr = ""
 		for att in self.att:
 			constr = constr + self.gen_fkcon(att) + ";\n"
 		return constr
 
-	#needed for set function
-	#returns dictionary of collname (column that references uid in some table) : tablename (table that is referenced)
 	def references_uid(self):
+		'''needed for set function
+		returns dictionary of collname (column that references uid in some table) : tablename (table that is referenced)'''
 		result = dict()
 		for conname in self.att:
 			refattributes = self.ratt[conname]
@@ -69,8 +65,8 @@ class Fks:
 				result[namecol] = self.tbl[conname]
 		return result
 
-	# FIXME: add comment
 	def embed_col(self):
+		'''return embed columns'''
 		for conname in self.att:
 			if (string.find(conname,'rembed_') == 0):
 				return self.att[conname]
