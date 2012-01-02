b02f5b3ab3a246f33f7147dece39331935b67bf9
Jan Kundrát <kundratj@fzu.cz>
2012-01-02 20:04:13 +0100

    qml: determine the output image size from the size of the root element
    The funny bit with the singleshot timer is required, otherwise the layout is all
    wrong -- fun, isn't it?

--- src/qml2image/Qml2Image.cpp
+++ src/qml2image/Qml2Image.cpp
@@ -1,19 +1,14 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
 #include <QtCore/QTimer>
 #include "Qml2Image.h"
 
 Qml2Image::Qml2Image(const QString &qmlFile, const QString &outFile_):
     generator(0), image(0), outFile(outFile_), qmlView(QUrl::fromLocalFile(qmlFile))
 {
-    qmlView.resize(800, 1100);
-    qmlView.viewport()->resize(qmlView.size());
-
     if (outFile.endsWith(".svg")) {
         generator = new QSvgGenerator();
         generator->setFileName(outFile);
-        generator->setSize(qmlView.size());
-    } else {
-        image = new QImage(qmlView.size(), QImage::Format_ARGB32);
     }
 
     connect(&qmlView, SIGNAL(statusChanged(QDeclarativeView::Status)), this, SLOT(maybeDone()));
@@ -27,15 +22,18 @@ void Qml2Image::maybeDone()
         qFatal("qml2image: QML viewer returned fatal error\n");
         QCoreApplication::exit(1);
     } else if (status == QDeclarativeView::Ready) {
-        slotSave();
+        QTimer::singleShot(0, this, SLOT(slotSave()));
     }
 }
 
 void Qml2Image::slotSave()
 {
+    qDebug() << "qml2image: current view size" << qmlView.size();
     if (generator) {
         painter.begin(generator);
+        generator->setSize(qmlView.size());
     } else {
+        image = new QImage(qmlView.size(), QImage::Format_ARGB32);
         painter.begin(image);
     }
 
@@ -46,6 +44,7 @@ void Qml2Image::slotSave()
     if (image) {
         image->save(outFile);
     }
+    //qmlView.show();
     QCoreApplication::exit(0);
 }
 
